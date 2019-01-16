8178ade313cae47dd98f4cecfd648155ddce2d22
George Mihai Lazu <glazu@deloittece.com>
2019-01-16 16:37:05 +0200

    CVSB-1280 * Fixed issue where searchIdentifiers shorter than 3 characters would cause the function to timeout. * Fixed issue where no error message would be returned when the searchIdentifier is longer than 21 characters.

--- src/functions/getTechRecords.js
+++ src/functions/getTechRecords.js
@@ -9,7 +9,7 @@ const getTechRecords = (event) => {
   const techRecordsService = new TechRecordsService(techRecordsDAO)
 
   let path = (process.env.BRANCH === 'local') ? event.path : event.pathParameters.proxy
-  let pathRegex = new RegExp('(\\w{3,}\\b)\\/tech-records\\/?(\\w+\\b)?')
+  let pathRegex = new RegExp('(\\w+\\b)\\/tech-records\\/?(\\w+\\b)?')
   let matches = pathRegex.exec(path)
 
   if (matches === null) { // No matches to regex found in URL
@@ -19,8 +19,8 @@ const getTechRecords = (event) => {
   const searchIdentifier = matches[1]
   const status = (matches[2]) ? matches[2] : 'current'
 
-  if (searchIdentifier.length < 3) {
-    return new HTTPResponse(400, 'The search identifier should be at least 3 characters long')
+  if (searchIdentifier.length < 3 || searchIdentifier.length > 21) {
+    return Promise.resolve(new HTTPResponse(400, 'The search identifier should be between 3 and 21 characters.'))
   }
 
   return techRecordsService.getTechRecordsList(searchIdentifier, status)
