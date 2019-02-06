aeb5c448e753a6389c25ff7d7602ea4b1669e43c
tealorg <43780372+tealorg@users.noreply.github.com>
2019-02-06 14:29:55 +0000

    Update Seed.groovy

--- Jenkinsfile/Seed.groovy
+++ Jenkinsfile/Seed.groovy
@@ -6,5 +6,46 @@ podTemplate(label: label, containers: [
         stage('checkout') {
             checkout scm
         }
+        
+        container('node'){    
+            
+            sh "cp -r /tmp/seed ."
+            
+            dir('seed'){
+                
+                stage ("npm deps") {
+                    sh "npm install"
+                }
+
+                stage ("credentials") {
+                    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'jenkins-np-iam', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                        sh "sls config credentials --provider aws --key ${AWS_ACCESS_KEY_ID} --secret ${AWS_SECRET_ACCESS_KEY}"
+                    }
+                }
+                
+                stage ("delete-table") {
+    
+                    sh "aws dynamodb delete-table --table-name cvs-local-technical-records --region=eu-west-1 || true"
+                    sh "aws dynamodb wait table-not-exists --table-name cvs-local-technical-records --region=eu-west-1"
+
+                }
+                
+                stage ("create-table") {
+                    sh '''
+                        aws dynamodb create-table \
+                        --region=eu-west-1 \
+                        --endpoint-url http://localhost:8003 \
+                        --table-name cvs-local-technical-records \
+                        --attribute-definitions AttributeName=partialVin,AttributeType=S AttributeName=vin,AttributeType=S AttributeName=primaryVrm,AttributeType=S --key-schema AttributeName=partialVin,KeyType=HASH AttributeName=vin,KeyType=RANGE --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 --global-secondary-indexes IndexName=VRMIndex,KeySchema=[{AttributeName=primaryVrm,KeyType=HASH}],Projection={ProjectionType=INCLUDE,NonKeyAttributes=[secondaryVrms,vin,techRecord]},ProvisionedThroughput="{ReadCapacityUnits=1,WriteCapacityUnits=1}"
+                        '''
+                    sh "aws dynamodb wait table-exists --table-name cvs-local-technical-records --region=eu-west-1"
+
+                }
+                
+                stage ("seed-table") {
+                        sh "./seed.js cvs-local-technical-records ../tests/resources/techRecords.json"
+                }
+            }
+        }
     }
 }
