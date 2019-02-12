15050cdd496dade2e9221a5dac58fb43e5c7baab
Nathan Blore <37145422+nblore@users.noreply.github.com>
2019-02-12 18:30:16 +0000

    Update Build.groovy

--- Jenkinsfile/Build.groovy
+++ Jenkinsfile/Build.groovy
@@ -75,7 +75,7 @@ podTemplate(label: label, containers: [
                                   secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
 
                     sh "aws s3 cp ${LBRANCH}.zip s3://cvs-services/technical-records/${LBRANCH}.zip"
-                    sh "echo sha256sum ${LBRANCH}.zip > ${LBRANCH}.sha256 && aws s3 cp ${LBRANCH}.sha256 s3://cvs-services/test-stations/${LBRANCH}.sha256"
+                    sh "echo sha256sum ${LBRANCH}.zip > ${LBRANCH}.sha256 && aws s3 cp ${LBRANCH}.sha256 s3://cvs-services/technical-records/${LBRANCH}.sha256"
 
                 }
             }
