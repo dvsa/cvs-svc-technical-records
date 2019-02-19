e0997c632c1248cf213f8e087ecf97862edc229f
nblore <nblore@deloitte.co.uk>
2019-02-19 18:00:51 +0000

    Fixed seed job

--- Jenkinsfile/Seed.groovy
+++ Jenkinsfile/Seed.groovy
@@ -37,11 +37,10 @@ podTemplate(label: label, containers: [
                     sh """
                         aws dynamodb create-table \
                         --region=eu-west-1 \
-                        --endpoint-url http://localhost:8003 \
-                        --table-name cvs-local-technical-records \
+                        --table-name cvs-${LBRANCH}-technical-records \
                         --attribute-definitions AttributeName=partialVin,AttributeType=S AttributeName=vin,AttributeType=S AttributeName=primaryVrm,AttributeType=S --key-schema AttributeName=partialVin,KeyType=HASH AttributeName=vin,KeyType=RANGE --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 --global-secondary-indexes IndexName=VRMIndex,KeySchema=[{AttributeName=primaryVrm,KeyType=HASH}],Projection={ProjectionType=INCLUDE,NonKeyAttributes=[secondaryVrms,vin,techRecord]},ProvisionedThroughput="{ReadCapacityUnits=1,WriteCapacityUnits=1}"
                         """
-                    sh "aws dynamodb wait table-exists --table-name cvs-local-technical-records --region=eu-west-1"
+                    sh "aws dynamodb wait table-exists --table-name cvs-${LBRANCH}-technical-records --region=eu-west-1"
                     sh "sleep 10"
                     sh """ aws dynamodb tag-resource \
                         --resource-arn arn:aws:dynamodb:eu-west-1:006106226016:table/cvs-${LBRANCH}-technical-records \
