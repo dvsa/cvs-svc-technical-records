cd7e4f065b20588d1313ed181ebbe091e4e5920e
khodder <khodder@gmail.com>
2019-09-10 17:33:23 +0100

    CVSB-3327 - rejigg test data to prevent it conflicting with automated tests

--- @Types/ITechRecordWrapper.d.ts
+++ @Types/ITechRecordWrapper.d.ts
@@ -1,10 +1,10 @@
 import ITechRecord from "./ITechRecord";
 
 export default interface ITechRecordWrapper {
-    primaryVrm: string;
-    secondaryVrms: string[];
+    primaryVrm?: string;
+    secondaryVrms?: string[];
     vin: string;
     partialVin: string;
-    trailerId: string;
+    trailerId?: string;
     techRecord: ITechRecord[];
 }
