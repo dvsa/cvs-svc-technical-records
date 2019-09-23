433178448563334fc0f01cd815af109b0dd69305
Catalin-Andrei Stancu <castancu@deloittece.com>
2019-09-23 17:40:15 +0300

    wrote integration tests, changed endpoints url structure + some validations on path params

--- tests/integration/TechRecordsFunction.intTest.ts
+++ tests/integration/TechRecordsFunction.intTest.ts
@@ -1,6 +1,9 @@
 import LambdaTester from "lambda-tester";
 import {getTechRecords as GetTechRecordsFunction} from "../../src/functions/getTechRecords";
 import {emptyDatabase, populateDatabase} from "../util/dbOperations";
+import ITechRecordWrapper from "../../@Types/ITechRecordWrapper";
+import {updateTechRecords as UpdateTechRecordsFunction} from "../../src/functions/updateTechRecords";
+import {postTechRecords as PostTechRecordsFunction} from "../../src/functions/postTechRecords";
 
 describe("getTechRecords", () => {
   beforeAll(async () => {
@@ -84,3 +87,272 @@ describe("getTechRecords", () => {
     });
   });
 });
+
+describe("postTechRecords", () => {
+  beforeAll(async () => {
+    jest.restoreAllMocks();
+    await emptyDatabase();
+  });
+  beforeEach(async () => {
+    await populateDatabase();
+  });
+  afterEach(async () => {
+    await emptyDatabase();
+  });
+  afterAll(async () => {
+    await populateDatabase();
+  });
+
+  const records = require("../resources/technical-records.json");
+  const techRecord: ITechRecordWrapper = records[0];
+
+  context("when trying to create a new vehicle", () => {
+    context("and the vehicle was found", () => {
+      it("should return error 400", () => {
+        return LambdaTester(PostTechRecordsFunction)
+            .event({
+              path: "/vehicles",
+              body: techRecord
+            })
+            .expectResolve((result: any) => {
+              expect(result.statusCode).toEqual(400);
+              expect(result.body).toEqual('"The conditional request failed"');
+            });
+      });
+    });
+
+    context("and the vehicle was not found", () => {
+      it("should return 201 created", () => {
+        techRecord.vin = Date.now().toString();
+        techRecord.partialVin = techRecord.vin.substr(techRecord.vin.length - 6);
+        techRecord.primaryVrm = Math.floor(100000 + Math.random() * 900000).toString();
+        return LambdaTester(PostTechRecordsFunction)
+            .event({
+              path: "/vehicles",
+              body: techRecord
+            })
+            .expectResolve((result: any) => {
+              expect(result.statusCode).toEqual(201);
+              expect(result.body).toEqual('"Technical Record created"');
+            });
+      });
+    });
+
+    context("and the techRecord array is empty", () => {
+      it("should return 400 invalid TechRecord", () => {
+        techRecord.vin = Date.now().toString();
+        techRecord.partialVin = techRecord.vin.substr(techRecord.vin.length - 6);
+        techRecord.techRecord = [];
+        return LambdaTester(PostTechRecordsFunction)
+            .event({
+              path: "/vehicles",
+              body: techRecord
+            })
+            .expectResolve((result: any) => {
+              expect(result.statusCode).toEqual(400);
+              expect(result.body).toEqual('"Body is not a valid TechRecord"');
+            });
+      });
+    });
+
+    context("and the event body is empty", () => {
+      it("should return 400 invalid TechRecord", () => {
+        techRecord.vin = Date.now().toString();
+        techRecord.partialVin = techRecord.vin.substr(techRecord.vin.length - 6);
+        techRecord.techRecord = [];
+        return LambdaTester(PostTechRecordsFunction)
+            .event({
+              path: "/vehicles",
+              body: undefined
+            })
+            .expectResolve((result: any) => {
+              expect(result.statusCode).toEqual(400);
+              expect(result.body).toEqual('"Body is not a valid TechRecord"');
+            });
+      });
+    });
+  });
+  beforeEach(() => {
+    jest.setTimeout(5000);
+  });
+  afterEach(() => {
+    jest.setTimeout(5000);
+  });
+});
+
+describe("updateTechRecords", () => {
+  beforeAll(async () => {
+    jest.restoreAllMocks();
+    await emptyDatabase();
+  });
+  beforeEach(async () => {
+    await populateDatabase();
+  });
+  afterEach(async () => {
+    await emptyDatabase();
+  });
+  afterAll(async () => {
+    await populateDatabase();
+  });
+
+  const records = require("../resources/technical-records.json");
+  const techRecord: ITechRecordWrapper = records[1];
+
+  context("when trying to update a vehicle", () => {
+    context("and the path parameter VIN is valid", () => {
+      context("and the vehicle was found", () => {
+        it("should return 200 and the updated vehicle", () => {
+          const vin = techRecord.vin;
+          delete techRecord.vin;
+          techRecord.techRecord[0].bodyType.description = "updated tech record";
+          techRecord.techRecord[0].grossGbWeight = 9900;
+          return LambdaTester(UpdateTechRecordsFunction)
+              .event({
+                path: `/vehicles/${vin}`,
+                pathParameters: {
+                  vin
+                },
+                body: techRecord
+              })
+              .expectResolve((result: any) => {
+                const updatedTechRes = JSON.parse(result.body);
+                expect(result.statusCode).toEqual(200);
+                expect(updatedTechRes.techRecord[0].bodyType.description).toEqual("updated tech record");
+                expect(updatedTechRes.techRecord[0].grossGbWeight).toEqual(9900);
+                expect(updatedTechRes).not.toHaveProperty("primaryVrm");
+                expect(updatedTechRes).not.toHaveProperty("partialVin");
+                expect(updatedTechRes).not.toHaveProperty("secondaryVrms");
+              });
+        });
+      });
+
+      context("and the vehicle was not found", () => {
+        it("should return 400 Bad Request", () => {
+          const vin = Date.now().toString();
+          techRecord.partialVin = techRecord.vin.substr(techRecord.vin.length - 6);
+          return LambdaTester(UpdateTechRecordsFunction)
+              .event({
+                path: `/vehicles/${vin}`,
+                pathParameters: {
+                  vin
+                },
+                body: techRecord
+              })
+              .expectResolve((result: any) => {
+                expect(result.statusCode).toEqual(400);
+                expect(result.body).toEqual('"The conditional request failed"');
+              });
+        });
+      });
+
+      context("and the techRecord array is empty", () => {
+        it("should return 400 invalid TechRecord", () => {
+          const vin = Date.now().toString();
+          techRecord.partialVin = vin.substr(vin.length - 6);
+          techRecord.techRecord = [];
+          return LambdaTester(UpdateTechRecordsFunction)
+              .event({
+                path: `/vehicles/${vin}`,
+                pathParameters: {
+                  vin
+                },
+                body: techRecord
+              })
+              .expectResolve((result: any) => {
+                expect(result.statusCode).toEqual(400);
+                expect(result.body).toEqual('"Body is not a valid TechRecord"');
+              });
+        });
+      });
+
+      context("and the event body is empty", () => {
+        it("should return 400 invalid TechRecord", () => {
+          const vin = Date.now().toString();
+          techRecord.partialVin = vin.substr(vin.length - 6);
+          techRecord.techRecord = [];
+          return LambdaTester(UpdateTechRecordsFunction)
+              .event({
+                path: `/vehicles/${vin}`,
+                pathParameters: {
+                  vin
+                },
+                body: undefined
+              })
+              .expectResolve((result: any) => {
+                expect(result.statusCode).toEqual(400);
+                expect(result.body).toEqual('"Body is not a valid TechRecord"');
+              });
+        });
+      });
+    });
+
+    context("and the path parameter VIN is invalid", () => {
+      context("and the path parameter VIN is null", () => {
+        it("should return 400 Invalid path parameter 'vin'", () => {
+          const vin = Date.now().toString();
+          techRecord.partialVin = vin.substr(vin.length - 6);
+          techRecord.techRecord = [];
+          return LambdaTester(UpdateTechRecordsFunction)
+              .event({
+                path: `/vehicles/${vin}`,
+                pathParameters: {
+                  vin: null
+                },
+                body: techRecord
+              })
+              .expectResolve((result: any) => {
+                expect(result.statusCode).toEqual(400);
+                expect(result.body).toEqual('"Invalid path parameter \'vin\'"');
+              });
+        });
+      });
+
+      context("and the path parameter VIN is shorter than 9 characters", () => {
+        it("should return 400 Invalid path parameter 'vin'", () => {
+          const vin = Date.now().toString();
+          techRecord.partialVin = vin.substr(vin.length - 6);
+          techRecord.techRecord = [];
+          return LambdaTester(UpdateTechRecordsFunction)
+              .event({
+                path: `/vehicles/${vin}`,
+                pathParameters: {
+                  vin: "ABCDEF5"
+                },
+                body: techRecord
+              })
+              .expectResolve((result: any) => {
+                expect(result.statusCode).toEqual(400);
+                expect(result.body).toEqual('"Invalid path parameter \'vin\'"');
+              });
+        });
+      });
+
+      context("and the path parameter VIN contains non alphanumeric characters", () => {
+        it("should return 400 Invalid path parameter 'vin'", () => {
+          const vin = Date.now().toString();
+          techRecord.partialVin = vin.substr(vin.length - 6);
+          techRecord.techRecord = [];
+          return LambdaTester(UpdateTechRecordsFunction)
+              .event({
+                path: `/vehicles/${vin}`,
+                pathParameters: {
+                  vin: "tech-r#cord$"
+                },
+                body: techRecord
+              })
+              .expectResolve((result: any) => {
+                expect(result.statusCode).toEqual(400);
+                expect(result.body).toEqual('"Invalid path parameter \'vin\'"');
+              });
+        });
+      });
+    });
+  });
+  beforeEach(() => {
+    jest.setTimeout(5000);
+  });
+  afterEach(() => {
+    jest.setTimeout(5000);
+  });
+});
+
--- tests/integration/techRecords.intTest.ts
+++ tests/integration/techRecords.intTest.ts
@@ -263,7 +263,237 @@ describe("techRecords", () => {
       });
     });
   });
+
+  describe("postTechRecords", () => {
+    context("when database is populated", () => {
+      beforeAll(async () => {
+        await emptyDatabase();
+      });
+
+      afterAll(async () => {
+        await populateDatabase();
+      });
+
+      context("when database is populated", () => {
+        beforeEach(async () => {
+          await populateDatabase();
+        });
+
+        afterEach(async () => {
+          await emptyDatabase();
+        });
+
+        // @ts-ignore
+        const newTechRec: ITechRecordWrapper = mockData[0];
+
+        context("and when trying to create a new vehicle", () => {
+          context("and the payload is valid", () => {
+            context("and that vehicle does not exist", () => {
+              it("should return status 201 Technical Record created", (done) => {
+                newTechRec.vin = Date.now().toString();
+                newTechRec.partialVin = newTechRec.vin.substr(newTechRec.vin.length - 6);
+                newTechRec.techRecord[0].bodyType.description = "New Tech Record";
+                newTechRec.primaryVrm = Math.floor(100000 + Math.random() * 900000).toString();
+                request.post("vehicles")
+                  .send(newTechRec)
+                  .end((err, res: any) => {
+                    expect(res.statusCode).toEqual(201);
+                    expect(res.headers["access-control-allow-origin"]).toEqual("*");
+                    expect(res.headers["access-control-allow-credentials"]).toEqual("true");
+                    expect(res.body).toEqual("Technical Record created");
+                    done();
+                  });
+              });
+            });
+
+            context("and that vehicle does exist", () => {
+              it("should return status 400 The conditional request failed", (done) => {
+                request.post("vehicles")
+                  .send(newTechRec)
+                  .end((err, res: any) => {
+                    expect(res.statusCode).toEqual(400);
+                    expect(res.headers["access-control-allow-origin"]).toEqual("*");
+                    expect(res.headers["access-control-allow-credentials"]).toEqual("true");
+                    expect(res.body).toEqual("The conditional request failed");
+                    done();
+                  });
+              });
+            });
+          });
+
+          context("and the payload is invalid", () => {
+            context("and the techRecord array is empty", () => {
+              it("should return status 400 The conditional request failed", (done) => {
+                newTechRec.techRecord = [];
+                request.post("vehicles")
+                  .send(newTechRec)
+                  .end((err, res: any) => {
+                    expect(res.statusCode).toEqual(400);
+                    expect(res.headers["access-control-allow-origin"]).toEqual("*");
+                    expect(res.headers["access-control-allow-credentials"]).toEqual("true");
+                    expect(res.body).toEqual("Body is not a valid TechRecord");
+                    done();
+                  });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+
+  describe("updateTechRecords", () => {
+    context("when database is populated", () => {
+      beforeAll(async () => {
+        await emptyDatabase();
+      });
+
+      afterAll(async () => {
+        await populateDatabase();
+      });
+
+      context("when database is populated", () => {
+        const convertToResponse = (dbObj: any) => { // Needed to convert an object from the database to a response object
+          const responseObj = Object.assign({}, dbObj);
+
+          // Adding primary and secondary VRMs in the same array
+          const vrms: any = [{isPrimary: true}];
+          if (responseObj.primaryVrm) {
+            vrms[0].vrm = responseObj.primaryVrm;
+          }
+
+          Object.assign(responseObj, {
+            vrms
+          });
+
+          // Cleaning up unneeded properties
+          delete responseObj.primaryVrm; // No longer needed
+          delete responseObj.secondaryVrms; // No longer needed
+          delete responseObj.partialVin; // No longer needed
+
+          return responseObj;
+        };
+        beforeEach(async () => {
+          await populateDatabase();
+        });
+
+        afterEach(async () => {
+          await emptyDatabase();
+        });
+
+        context("and when trying to update a vehicle", () => {
+          context("and the path parameter VIN is valid", () => {
+            context("and that vehicle does exist", () => {
+              it("should return status 200 and the updated vehicle", (done) => {
+                // @ts-ignore
+                const techRec: ITechRecordWrapper = mockData[1];
+                techRec.techRecord[0].bodyType.description = "Updated Tech Record";
+                techRec.techRecord[0].grossGbWeight = 5678;
+                request.put(`vehicles/${techRec.vin}`)
+                  .send(techRec)
+                  .end((err, res: any) => {
+                    expect(res.statusCode).toEqual(200);
+                    expect(res.headers["access-control-allow-origin"]).toEqual("*");
+                    expect(res.headers["access-control-allow-credentials"]).toEqual("true");
+                    expect(res.body).toEqual(convertToResponse(techRec));
+                    done();
+                  });
+              });
+            });
+
+            context("and that vehicle does not exist", () => {
+              it("should return error status 400 The conditional request failed", (done) => {
+                // @ts-ignore
+                const techRec: ITechRecordWrapper = mockData[1];
+                techRec.vin = Date.now().toString();
+                techRec.partialVin = techRec.vin.substr(techRec.vin.length - 6);
+                techRec.primaryVrm = Math.floor(100000 + Math.random() * 900000).toString();
+                techRec.techRecord[0].bodyType.description = "Updated Tech Record";
+                techRec.techRecord[0].grossGbWeight = 5678;
+                request.put(`vehicles/${techRec.vin}`)
+                  .send(techRec)
+                  .end((err, res: any) => {
+                    expect(res.statusCode).toEqual(400);
+                    expect(res.headers["access-control-allow-origin"]).toEqual("*");
+                    expect(res.headers["access-control-allow-credentials"]).toEqual("true");
+                    expect(res.body).toEqual("The conditional request failed");
+                    done();
+                  });
+              });
+            });
+
+            context("and the techRecord array is empty", () => {
+              it("should return error status 400 Body is not a valid TechRecord", (done) => {
+                // @ts-ignore
+                const techRec: ITechRecordWrapper = mockData[1];
+                techRec.techRecord = [];
+                request.put(`vehicles/${techRec.vin}`)
+                  .send(techRec)
+                  .end((err, res: any) => {
+                    expect(res.statusCode).toEqual(400);
+                    expect(res.headers["access-control-allow-origin"]).toEqual("*");
+                    expect(res.headers["access-control-allow-credentials"]).toEqual("true");
+                    expect(res.body).toEqual("Body is not a valid TechRecord");
+                    done();
+                  });
+              });
+            });
+          });
+
+          context("and the path parameter VIN is invalid", () => {
+            context("and the path parameter VIN is null", () => {
+              it("should return 400 Invalid path parameter 'vin'", (done) => {
+                // @ts-ignore
+                const techRec: ITechRecordWrapper = mockData[1];
+                techRec.techRecord = [];
+                request.put(`vehicles/null`)
+                  .send(techRec)
+                  .end((err, res: any) => {
+                    expect(res.statusCode).toEqual(400);
+                    expect(res.headers["access-control-allow-origin"]).toEqual("*");
+                    expect(res.headers["access-control-allow-credentials"]).toEqual("true");
+                    expect(res.body).toEqual("Invalid path parameter \'vin\'");
+                    done();
+                  });
+              });
+            });
+
+            context("and the path parameter VIN is shorter than 9 characters", () => {
+              it("should return 400 Invalid path parameter 'vin'", (done) => {
+                // @ts-ignore
+                const techRec: ITechRecordWrapper = mockData[1];
+                techRec.techRecord = [];
+                request.put(`vehicles/ABCDEF5`)
+                  .send(techRec)
+                  .end((err, res: any) => {
+                    expect(res.statusCode).toEqual(400);
+                    expect(res.headers["access-control-allow-origin"]).toEqual("*");
+                    expect(res.headers["access-control-allow-credentials"]).toEqual("true");
+                    expect(res.body).toEqual("Invalid path parameter \'vin\'");
+                    done();
+                  });
+              });
+            });
+
+            context("and the path parameter VIN contains non alphanumeric characters", () => {
+              it("should return 400 Invalid path parameter 'vin'", (done) => {
+                // @ts-ignore
+                const techRec: ITechRecordWrapper = mockData[1];
+                techRec.techRecord = [];
+                request.put(`vehicles/t@ch-r#cord$`)
+                  .send(techRec)
+                  .end((err, res: any) => {
+                    expect(res.statusCode).toEqual(400);
+                    expect(res.headers["access-control-allow-origin"]).toEqual("*");
+                    expect(res.headers["access-control-allow-credentials"]).toEqual("true");
+                    expect(res.body).toEqual("Invalid path parameter \'vin\'");
+                    done();
+                  });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
 });
-
-
-
