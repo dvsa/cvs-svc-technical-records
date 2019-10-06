9475858eb21dd465d00f2cc4e28e41300159bdef
Arup Banerjee <arupdvsa@alanaamy.net>
2019-10-06 14:53:12 +0100

    added specific tech record and tests intermediate

--- tests/unit/handler.unitTest.ts
+++ tests/unit/handler.unitTest.ts
@@ -1,134 +1,89 @@
-import sinon from "sinon";
-import {handler} from "../../src/handler";
-import * as getTechRecords from "../../src/functions/getTechRecords";
+import { handler } from "../../src/handler";
 import Configuration from "../../src/utils/Configuration";
 import HTTPResponse from "../../src/models/HTTPResponse";
 import mockContext from "aws-lambda-mock-context";
 import event from "../resources/event.json";
-const sandbox = sinon.createSandbox();
-
-describe("The lambda function handler", () => {
-  const ctx = mockContext();
-  afterAll(() => {
-    sandbox.restore();
-  });
-  context("With correct Config", () => {
-    context("should correctly handle incoming events", () => {
-      it("should call functions with correct event payload", async () => {
-        // Specify your event, with correct path, payload etc
-        const vehicleRecordEvent = event;
-
-        // Stub out the actual functions
-        const getTechRecordsStub = sandbox.stub(getTechRecords);
-        getTechRecordsStub.getTechRecords.resolves(new HTTPResponse(200, {}));
-
-        const result = await handler(vehicleRecordEvent, ctx);
-        expect(result.statusCode).toEqual(200);
-        sandbox.assert.called(getTechRecordsStub.getTechRecords);
-      });
-
-      it("should return error on empty event", async () => {
-        const result = await handler(null, ctx);
-
-        expect(result).toBeInstanceOf(HTTPResponse);
-        expect(result.statusCode).toEqual(400);
-        expect(result.body).toEqual(JSON.stringify("AWS event is empty. Check your test event."));
-      });
-
-      it("should return error on invalid body json", async () => {
-        const invalidBodyEvent = Object.assign({}, event);
-        invalidBodyEvent.body = '{"hello":}';
-
-        const result = await handler(invalidBodyEvent, ctx);
-        expect(result).toBeInstanceOf(HTTPResponse);
-        expect(result.statusCode).toEqual(400);
-        expect(result.body).toEqual(JSON.stringify("Body is not a valid JSON."));
-      });
-
-      it("should return a Route Not Found error on invalid path", async () => {
-        const invalidPathEvent = Object.assign({}, event);
-        // invalidPathEvent.body = ""
-        invalidPathEvent.path = "/vehicles/123/doesntExist";
-
-        const result = await handler(invalidPathEvent, ctx);
-        expect(result.statusCode).toEqual(400);
-        expect(result.body).toStrictEqual(JSON.stringify({ error: `Route ${invalidPathEvent.httpMethod} ${invalidPathEvent.path} was not found.` }));
-      });
-    });
-  });
-
-  context("With no routes defined in config", () => {
-    it("should return a Route Not Found error", async () => {
-      // Stub Config getFunctions method and return empty array instead
-      const configStub = sandbox.stub(Configuration.prototype, "getFunctions").returns([]);
-
-      const result = await handler(event, ctx);
-      expect(result.statusCode).toEqual(400);
-      expect(result.body).toStrictEqual(JSON.stringify({ error: `Route ${event.httpMethod} ${event.path} was not found.` }));
-      configStub.restore();
-    });
-  });
+import TechRecordsService from "../../src/services/TechRecordsService";
+jest.mock("../../src/services/TechRecordsService");
+const opts = Object.assign({
+    timeout: 0.2
 });
 
-describe("The configuration service", () => {
-  context("with good config file", () => {
-    it("should return local versions of the config if specified", () => {
-      process.env.BRANCH = "local";
-      const configService = Configuration.getInstance();
-      const functions = configService.getFunctions();
-      expect(functions.length).toEqual(1);
-      expect(functions[0].name).toEqual("getTechRecords");
+describe("The lambda function handler", () => {
+    context("With correct Config", () => {
+        context("should correctly handle incoming events", () => {
+            it("should call functions with correct event payload", async () => {
+                // Specify your event, with correct path, payload etc
+                const vehicleRecordEvent = {
+                    path: "/vehicles/YV31MEC18GA011944/tech-records",
+                    pathParameters: null,
+                    resource: "/vehicles",
+                    httpMethod: "GET",
+                    queryStringParameters: null
+                };
 
-      const DBConfig = configService.getDynamoDBConfig();
-      expect(DBConfig).toEqual(configService.getConfig().dynamodb.local);
+                const ctx: any = mockContext(opts);
 
-      // No Endpoints for this service
+                // Stub out the actual functions
+                TechRecordsService.prototype.getTechRecordsList = jest.fn().mockImplementation(() => {
+                    return Promise.resolve(new HTTPResponse(200, {}));
+                });
+
+                const result = await handler(vehicleRecordEvent, ctx);
+                ctx.succeed(result);
+                expect(result.statusCode).toEqual(200);
+                expect(TechRecordsService.prototype.getTechRecordsList).toHaveBeenCalled();
+            });
+
+            it("should return error on empty event", async () => {
+                let ctx: any = mockContext(opts);
+                const result = await handler(null, ctx);
+                ctx.succeed(result);
+                ctx = null;
+
+                expect(result).toBeInstanceOf(HTTPResponse);
+                expect(result.statusCode).toEqual(400);
+                expect(result.body).toEqual(JSON.stringify("AWS event is empty. Check your test event."));
+            });
+
+            it("should return error on invalid body json", async () => {
+                const invalidBodyEvent = Object.assign({}, event);
+                invalidBodyEvent.body = '{"hello":}';
+
+                const ctx: any = mockContext(opts);
+                const result = await handler(invalidBodyEvent, ctx);
+                expect(result).toBeInstanceOf(HTTPResponse);
+                expect(result.statusCode).toEqual(400);
+                expect(result.body).toEqual(JSON.stringify("Body is not a valid JSON."));
+            });
+
+            it("should return a Route Not Found error on invalid path", async () => {
+                const invalidPathEvent = Object.assign({}, event);
+                invalidPathEvent.path = "/vehicles/123/doesntExist";
+
+                let ctx: any = mockContext(opts);
+                const result = await handler(invalidPathEvent, ctx);
+                ctx.succeed(result);
+                ctx = null;
+                expect(result.statusCode).toEqual(400);
+                expect(result.body).toStrictEqual(JSON.stringify({ error: `Route ${invalidPathEvent.httpMethod} ${invalidPathEvent.path} was not found.` }));
+            });
+        });
     });
 
-    it("should return local-global versions of the config if specified", () => {
-      process.env.BRANCH = "local-global";
-      const configService = Configuration.getInstance();
-      const functions = configService.getFunctions();
-      expect(functions.length).toEqual(1);
-      expect(functions[0].name).toEqual("getTechRecords");
+    context("With no routes defined in config", () => {
+        it("should return a Route Not Found error", async () => {
 
-      const DBConfig = configService.getDynamoDBConfig();
-      expect(DBConfig).toEqual(configService.getConfig().dynamodb["local-global"]);
-
-      // No Endpoints for this service
+            const getFunctions = Configuration.prototype.getFunctions;
+            Configuration.prototype.getFunctions = jest.fn().mockImplementation(() => []);
+            const eventNoRoute = { httpMethod: "GET", path: "" };
+            let ctx: any = mockContext(opts);
+            const result = await handler(eventNoRoute, ctx);
+            ctx.succeed(result);
+            ctx = null;
+            expect(result.statusCode).toEqual(400);
+            expect(result.body).toEqual(JSON.stringify({ error: `Route ${eventNoRoute.httpMethod} ${eventNoRoute.path} was not found.` }));
+            Configuration.prototype.getFunctions = getFunctions;
+        });
     });
-
-    it("should return remote versions of the config by default", () => {
-      process.env.BRANCH = "CVSB-XXX";
-      const configService = Configuration.getInstance();
-      const functions = configService.getFunctions();
-      expect(functions.length).toEqual(1);
-      expect(functions[0].name).toEqual("getTechRecords");
-
-      const DBConfig = configService.getDynamoDBConfig();
-      expect(DBConfig).toEqual(configService.getConfig().dynamodb.remote);
-
-      // No Endpoints for this service
-    });
-  });
-
-  context("with bad config file", () => {
-    it("should return an error for missing functions from getFunctions", () => {
-      const config = new Configuration("../../tests/resources/badConfig.yml");
-      try {
-        config.getFunctions();
-      } catch (e) {
-        expect(e.message).toEqual("Functions were not defined in the config file.");
-      }
-    });
-
-    it("should return an error for missing DB Config from getDynamoDBConfig", () => {
-      const config = new Configuration("../../tests/resources/badConfig.yml");
-      try {
-        config.getDynamoDBConfig();
-      } catch (e) {
-        expect(e.message).toEqual("DynamoDB config is not defined in the config file.");
-      }
-    });
-  });
 });
--- tests/util/dbOperations.ts
+++ tests/util/dbOperations.ts
@@ -1,8 +1,9 @@
 import TechRecordsDAO from "../../src/models/TechRecordsDAO";
 import techRecords from "../resources/technical-records.json";
+import * as _ from "lodash";
 
 export const populateDatabase = async () => {
-    const mockBuffer = [...techRecords];
+    const mockBuffer = _.cloneDeep(techRecords);
     const techRecordsDAO = new TechRecordsDAO();
     const batches = [];
     while (mockBuffer.length > 0) {
@@ -17,7 +18,7 @@ export const populateDatabase = async () => {
 
 export const emptyDatabase = async () => {
     const techRecordsDAO = new TechRecordsDAO();
-    const mockBuffer = [...techRecords].map((record) => [record.partialVin, record.vin]);
+    const mockBuffer = _.cloneDeep(techRecords).map((record) => [record.partialVin, record.vin]);
     const batches = [];
     while (mockBuffer.length > 0) {
         batches.push(mockBuffer.splice(0, 25));
@@ -26,4 +27,35 @@ export const emptyDatabase = async () => {
     for (const batch of batches) {
         await techRecordsDAO.deleteMultiple(batch);
     }
+};
+
+export const convertToResponse = (dbObj: any) => { // Needed to convert an object from the database to a response object
+    const responseObj = Object.assign({}, dbObj);
+
+    // Adding primary and secondary VRMs in the same array
+    const vrms: any = [{ isPrimary: true }];
+    if (responseObj.primaryVrm) { vrms[0].vrm = responseObj.primaryVrm; }
+
+    Object.assign(responseObj, {
+        vrms
+    });
+
+    // Cleaning up unneeded properties
+    delete responseObj.primaryVrm; // No longer needed
+    delete responseObj.secondaryVrms; // No longer needed
+    delete responseObj.partialVin; // No longer needed
+
+    return responseObj;
+};
+
+export const convertTo7051Response = (dbObj: any, resolvedRecordIndex: number) => { // Needed to convert an object from the database to a response object
+    const responseObj = convertToResponse(_.cloneDeep(dbObj));
+
+
+    // replace techRecord with resolvedRecordIndex
+    const resolvedRecord = _.cloneDeep(responseObj.techRecord[resolvedRecordIndex]);
+    responseObj.techRecord.length = 0;
+    responseObj.techRecord.push(resolvedRecord);
+
+    return responseObj;
 };
