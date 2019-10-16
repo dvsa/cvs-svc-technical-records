5fc13561a77e7c82cce7dc576980f9aeba1c6592
Catalin-Andrei Stancu <castancu@deloittece.com>
2019-10-16 17:16:14 +0300

    added jest-cucumber and template for BDD

---
+++ tests/features/7051.ACs.feature
@@ -0,0 +1,21 @@
+Feature: Retrieve (v7) - Update the endpoint to retrieve the technical records
+  Scenario: AC1.1 API Consumer retrieve the Vehicle Technical Records for - query parameter "status" not provided & vehicle has both "current" and "provisional" technical records
+    Given I am an API Consumer
+    When I send a request to AWS_CVS_DOMAIN/vehicles/{searchIdentifier}/tech-records
+    And for the identified vehicle in the database there is a Technical Record with the "statusCode" = "current"
+    And for the identified vehicle in the database there is a Technical Record with the "statusCode" = "provisional"
+    Then for the query parameter "status", the default value "provisional_over_current" will be taken into account
+    And the system returns a body message containing a single CompleteTechRecord
+    And the statusCode of the Technical Records "provisional"
+    And the system returns an HTTP status code 200 OK
+
+
+  Scenario: AC1.2 API Consumer retrieve the Vehicle Technical Records for - query parameter "status" not provided & vehicle has only one "current" OR "provisional" technical record
+    Given I am an API Consumer
+    When I send a request to AWS_CVS_DOMAIN/vehicles/{searchIdentifier}/tech-records
+    And the query parameter "status" is not provided
+    And for the identified vehicle in the database there is only one "current" OR "provisional" Technical Record - not both of them at the same time
+    Then for the query parameter "status", the default value "provisional_over_current" will be taken into account
+    And the system returns a body message containing a single CompleteTechRecord
+    And the specific Technical Record found in database is returned - "current" or "provisional" as it is in the database
+    And the system returns an HTTP status code 200 OK
---
+++ tests/features/step_definitions/7051.ACs.feature.steps.intTest.ts
@@ -0,0 +1,91 @@
+import {defineFeature, loadFeature} from 'jest-cucumber';
+import supertest from "supertest";
+import path from 'path';
+
+const url = "http://localhost:3005/";
+const request = supertest(url);
+import {convertTo7051Response} from "../../util/dbOperations";
+import mockData from "../../resources/technical-records.json";
+import * as _ from "lodash";
+
+const feature = loadFeature(path.resolve(__dirname, "../7051.ACs.feature"));
+
+defineFeature(feature, test => {
+  test('AC1.1 API Consumer retrieve the Vehicle Technical Records for - ' +
+    'query parameter "status" not provided & vehicle has both "current" and "provisional" technical records', ({given, when, then, and}) => {
+    let requestUrl: string;
+    let response: any;
+    let expectedResponse: any;
+    given('I am an API Consumer', () => {
+      requestUrl = 'vehicles/YV31MEC18GA011911/tech-records';
+    });
+    when('I send a request to AWS_CVS_DOMAIN/vehicles/{searchIdentifier}/tech-records', async () => {
+      response = await request.get(requestUrl);
+    });
+    and('for the identified vehicle in the database there is a Technical Record with the "statusCode" = "current"', () => {
+      const isStatusPresent = isStatusCodePresent(mockData[9], "current");
+      expect(isStatusPresent).toBe(true);
+    });
+    and('for the identified vehicle in the database there is a Technical Record with the "statusCode" = "provisional"', () => {
+      const isStatusPresent = isStatusCodePresent(mockData[9], "provisional");
+      expect(isStatusPresent).toBe(true);
+    });
+    then('for the query parameter "status", the default value "provisional_over_current" will be taken into account', () => {
+      expectedResponse = convertTo7051Response(_.cloneDeep(mockData[9]), 1);
+    });
+    and('the system returns a body message containing a single CompleteTechRecord', () => {
+      expect(expectedResponse).toEqual(response.body);
+    });
+    and('the statusCode of the Technical Records "provisional"', () => {
+      const isStatusPresent = isStatusCodePresent(response.body, "provisional");
+      expect(isStatusPresent).toBe(true);
+    });
+    and('the system returns an HTTP status code 200 OK', () => {
+      expect(response.status).toEqual(200);
+    });
+  });
+
+  test('AC1.2 API Consumer retrieve the Vehicle Technical Records for - ' +
+    'query parameter "status" not provided & vehicle has only one "current" OR "provisional" technical record', ({given, when, then, and}) => {
+    let requestUrl: string;
+    let response: any;
+    let expectedResponse: any;
+    given('I am an API Consumer', () => {
+      requestUrl = 'vehicles/021430/tech-records';
+    });
+    when('I send a request to AWS_CVS_DOMAIN/vehicles/{searchIdentifier}/tech-records', async () => {
+      response = await request.get(requestUrl);
+    });
+    and('the query parameter "status" is not provided', () => {});
+    and('for the identified vehicle in the database there is only one "current" OR "provisional" Technical Record - not both of them at the same time', () => {
+      const isProvisional = isStatusCodePresent(mockData[3], "provisional");
+      const isCurrent = isStatusCodePresent(mockData[3], "current");
+      expect((isProvisional || isCurrent)).toBe(true);
+      expect((isProvisional && isCurrent)).toBe(false);
+    });
+    then('for the query parameter "status", the default value "provisional_over_current" will be taken into account', () => {
+      expectedResponse = convertTo7051Response(_.cloneDeep(mockData[3]), 0);
+    });
+    and('the system returns a body message containing a single CompleteTechRecord', () => {
+      expect(expectedResponse).toEqual(response.body);
+    });
+    and('the specific Technical Record found in database is returned - "current" or "provisional" as it is in the database', () => {
+      const isProvisional = isStatusCodePresent(response.body, "provisional");
+      const isCurrent = isStatusCodePresent(response.body, "current");
+      expect((isProvisional || isCurrent)).toBe(true);
+    });
+    and('the system returns an HTTP status code 200 OK', () => {
+      expect(response.status).toEqual(200);
+    });
+  });
+});
+
+const isStatusCodePresent = (completeTechRecord: any, status: string) => {
+  let isStatusCodePresent = false;
+  completeTechRecord.techRecord.forEach((record: any) => {
+    if (record.statusCode === status) {
+      isStatusCodePresent = true;
+    }
+  });
+  return isStatusCodePresent;
+};
