920bfc12837650f8a9623efb8f016e9123af552b
Catalin-Andrei Stancu <castancu@deloittece.com>
2019-10-17 15:43:34 +0300

    fixed timeout issues on jenkins

--- tests/features/step_definitions/7743.ACs.feature.steps.intTest.ts
+++ tests/features/step_definitions/7743.ACs.feature.steps.intTest.ts
@@ -5,15 +5,21 @@ import path from 'path';
 const url = "http://localhost:3005/";
 const request = supertest(url);
 import mockData from "../../resources/technical-records.json";
+import mockContext from "aws-lambda-mock-context";
+
+const opts = Object.assign({
+  timeout: 0.5
+});
 
 const feature = loadFeature(path.resolve(__dirname, "../7743.ACs.feature"));
 
 defineFeature(feature, test => {
+  let ctx: any = mockContext(opts);
   test('AC1. Backend Service Correctly Interprets The "status" value of "all"', ({given, when, then, and}) => {
     let requestUrl: string;
     let response: any;
     given('I am a consumer of the vehicles API', () => {
-      requestUrl = 'vehicles/C47WLL/tech-records?status=all'
+      requestUrl = 'vehicles/C47WLL/tech-records?status=all';
     });
     when('I call the vehicles API passing a value of "all" for the "status" (in addition to the VIN/VRM)', async () => {
       response = await request.get(requestUrl);
@@ -23,4 +29,6 @@ defineFeature(feature, test => {
       expect(response.body.techRecord.length).toEqual(mockData[8].techRecord.length);
     });
   });
+  ctx.succeed('done');
+  ctx = null;
 });
