9096031a2d94e39be293d9f69276c9fa13d5df7e
Alexandru Goncearenco <agoncearenco@deloitte.co.uk>
2019-12-17 12:49:37 +0000

    CVSB-4867 added status update check

--- src/functions/updateTechRecordStatus.ts
+++ src/functions/updateTechRecordStatus.ts
@@ -2,28 +2,27 @@ import TechRecordsDAO from "../models/TechRecordsDAO";
 import TechRecordsService from "../services/TechRecordsService";
 import HTTPResponse from "../models/HTTPResponse";
 import {APIGatewayEvent} from "aws-lambda";
-import {STATUS} from "../assets/Enums";
+import {STATUS, HTTPRESPONSE} from "../assets/Enums";
 import S3BucketService from "../services/S3BucketService";
 import { S3 } from "aws-sdk";
 
 export async function updateTechRecordStatus(event: APIGatewayEvent) {
     const s3BucketService = new S3BucketService(new S3());
     const techRecordsService = new TechRecordsService(new TechRecordsDAO(), s3BucketService);
-    const ONLY_DIGITS_AND_NUMBERS: RegExp = /^[A-Za-z0-9]+$/;
 
     const vin = event.pathParameters!.vin;
-    console.log(STATUS[event.queryStringParameters!.newStatus as keyof typeof STATUS]);
+    const testStatus: string = event.queryStringParameters!.testStatus;
+    const testResult: string = event.queryStringParameters!.testResult;
+    const testTypeId: string = event.queryStringParameters!.testTypeId;
     const newStatus = event.queryStringParameters ? STATUS[event.queryStringParameters.newStatus as keyof typeof STATUS] : undefined;
 
-    if (!vin || !ONLY_DIGITS_AND_NUMBERS.test(vin) || vin.length < 9) {
-        return Promise.resolve(new HTTPResponse(400, "Invalid path parameter 'vin'"));
+    if (!TechRecordsService.isStatusUpdateRequired(testStatus, testResult, testTypeId)) {
+        return new HTTPResponse(200, HTTPRESPONSE.NO_STATUS_UPDATE_REQUIRED);
     }
-
     try {
-        const updatedTechRec = await techRecordsService.updateTechRecordStatus(vin, newStatus );
+        const updatedTechRec = await techRecordsService.updateTechRecordStatusCode(vin, newStatus );
         return new HTTPResponse(200, updatedTechRec);
     } catch (error) {
         return new HTTPResponse(error.statusCode, error.body);
     }
 }
-
