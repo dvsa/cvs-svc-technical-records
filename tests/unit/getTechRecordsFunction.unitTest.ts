671e8c34591bd2205a3f3f66f6f2ab44d2c4e907
khodder <khodder@gmail.com>
2019-12-17 13:58:41 +0000

    CVSB-9335 - fix broken status defaulting

--- tests/unit/getTechRecordsFunction.unitTest.ts
+++ tests/unit/getTechRecordsFunction.unitTest.ts
@@ -5,39 +5,72 @@ jest.mock("../../src/services/TechRecordsService");
 describe("getTechRecords Function", () => {
   describe("parsing query parameters", () => {
     beforeEach(() => {
-      jest.resetAllMocks();
+      jest.restoreAllMocks();
     });
-    it("defaults missing searchCriteria to ALL", () => {
+    describe("missing query params", () => {
       const event = {
         pathParameters: {
           searchIdentifier: "XMGDE02FS0H012345"
         },
-        queryStringParameters: {
-          status: "current",
-          metadata: "true"
-        }
       };
       const mock = jest.fn().mockResolvedValue([]);
       TechRecordsService.prototype.getTechRecordsList = mock;
       getTechRecords(event);
-      expect(mock.mock.calls[0]).toContain("all");
+      it("defaults missing searchCriteria to ALL", () => {
+        expect(mock.mock.calls[0][2]).toEqual("all");
+      });
+      it("defaults missing status to PROVISIONAL_OVER_CURRENT", () => {
+        expect(mock.mock.calls[0][1]).toEqual("provisional_over_current");
+      });
     });
 
-    it("passes on specified searchCriteria value", () => {
-      const event = {
-        pathParameters: {
-          searchIdentifier: "XMGDE02FS0H012345"
-        },
-        queryStringParameters: {
-          status: "current",
-          metadata: "true",
-          searchCriteria: "vrm"
-        }
-      };
-      const mock = jest.fn().mockResolvedValue([]);
-      TechRecordsService.prototype.getTechRecordsList = mock;
-      getTechRecords(event);
-      expect(mock.mock.calls[0]).toContain("vrm");
+    describe("with individual params", () => {
+      it("passes on specified searchCriteria value", () => {
+        const event = {
+          pathParameters: {
+            searchIdentifier: "XMGDE02FS0H012345"
+          },
+          queryStringParameters: {
+            searchCriteria: "vrm"
+          }
+        };
+        const mock = jest.fn().mockResolvedValue([]);
+        TechRecordsService.prototype.getTechRecordsList = mock;
+        getTechRecords(event);
+        expect(mock.mock.calls[0][2]).toEqual("vrm");
+      });
+      it("passes on specified status value", () => {
+        const event = {
+          pathParameters: {
+            searchIdentifier: "XMGDE02FS0H012345"
+          },
+          queryStringParameters: {
+            status: "current"
+          }
+        };
+        const mock = jest.fn().mockResolvedValue([]);
+        TechRecordsService.prototype.getTechRecordsList = mock;
+        getTechRecords(event);
+        expect(mock.mock.calls[0][1]).toEqual("current");
+      });
+    });
+    describe("with multiple params", () => {
+      it("passes on specified searchCriteria and status value", () => {
+        const event = {
+          pathParameters: {
+            searchIdentifier: "XMGDE02FS0H012345"
+          },
+          queryStringParameters: {
+            searchCriteria: "searchOption",
+            status: "rhubarb"
+          }
+        };
+        const mock = jest.fn().mockResolvedValue([]);
+        TechRecordsService.prototype.getTechRecordsList = mock;
+        getTechRecords(event);
+        expect(mock.mock.calls[0][1]).toEqual("rhubarb");
+        expect(mock.mock.calls[0][2]).toEqual("searchOption");
+      });
     });
   });
 });
