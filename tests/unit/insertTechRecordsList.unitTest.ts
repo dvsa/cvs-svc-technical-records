d3e3deb199a18ea54b9ff1c481a6bc416e29f100
khodder <khodder@gmail.com>
2019-12-18 10:35:36 +0000

    CVSB-9335 - add requested record

--- tests/unit/insertTechRecordsList.unitTest.ts
+++ tests/unit/insertTechRecordsList.unitTest.ts
@@ -29,7 +29,7 @@ describe("insertTechRecordsList", () => {
 
       // @ts-ignore
       const data: ITechRecord[] = await techRecordsService.insertTechRecordsList(records);
-      expect(data.length).toEqual(43);
+      expect(data.length).toEqual(44);
     });
 
     it("should return nothing", async () => {
