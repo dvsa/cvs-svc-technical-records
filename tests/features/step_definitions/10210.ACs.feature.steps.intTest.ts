76ff8b57d10d907a93e73e1d38c2919dea28c4ff
Catalin-Andrei Stancu <castancu@deloittece.com>
2019-12-20 15:38:06 +0200

    AC tests

---
+++ tests/features/step_definitions/10210.ACs.feature.steps.intTest.ts
@@ -0,0 +1,144 @@
+import {defineFeature, loadFeature} from 'jest-cucumber';
+import supertest from "supertest";
+import path from 'path';
+import mockData from "../../resources/technical-records.json";
+import mockContext from "aws-lambda-mock-context";
+import {emptyDatabase, populateDatabase} from "../../util/dbOperations";
+import {UPDATE_TYPE} from "../../../src/assets/Enums";
+import {validatePayload} from "../../../src/utils/PayloadValidation";
+import {cloneDeep} from "lodash";
+
+const url = "http://localhost:3005/";
+const request = supertest(url);
+const opts = Object.assign({
+  timeout: 1.5
+});
+
+const feature = loadFeature(path.resolve(__dirname, "../10210.ACs.feature"));
+
+defineFeature(feature, test => {
+  beforeAll(async () => {
+    await emptyDatabase();
+  });
+
+  beforeEach(async () => {
+    await populateDatabase();
+  });
+
+  afterEach(async () => {
+    await emptyDatabase();
+  });
+
+  afterAll(async () => {
+    await populateDatabase();
+  });
+
+  test('AC1. POST: Attempt to create a new vehicle without a mandatory field', ({given, when, then}) => {
+    let ctx: any = mockContext(opts);
+
+    let requestUrl: string;
+    let response: any;
+
+    given('I am a consumer of the vehicles API', () => {
+      requestUrl = 'vehicles';
+    });
+    when('I call the vehicles API via the POST method without a mandatory field in the request body', async () => {
+      const postPayload = createPOSTPayload();
+      delete postPayload.techRecord[0].manufactureYear;
+      response = await request.post(requestUrl).send(postPayload);
+    });
+    then('I am given the 400 error code', () => {
+      expect(response.status).toEqual(400);
+      expect(response.body[0].message).toEqual('"manufactureYear" is required');
+    });
+    ctx.succeed('done');
+    ctx = null;
+  });
+
+  test('AC2. POST: Attempt to create a new vehicle with a not applicable field', ({given, when, then}) => {
+    let ctx: any = mockContext(opts);
+
+    let requestUrl: string;
+    let response: any;
+
+    given('I am a consumer of the vehicles API', () => {
+      requestUrl = 'vehicles';
+    });
+    when('I call the vehicles API via the POST method with at least one not applicable field (for example, a "PSV only" field, onto a HGV)', async () => {
+      const postPayload = createPOSTPayload();
+      postPayload.techRecord[0].unladenWeight = 0;
+      response = await request.post(requestUrl).send(postPayload);
+    });
+    then('I am given the 400 error code', () => {
+      expect(response.status).toEqual(400);
+      expect(response.body[0].message).toEqual('"unladenWeight" is not allowed');
+    });
+    ctx.succeed('done');
+    ctx = null;
+  });
+
+  test('AC3. POST: Attempt to create a new vehicle with unexpected values for a field that accepts only specific values', ({given, when, then}) => {
+    let ctx: any = mockContext(opts);
+
+    let requestUrl: string;
+    let response: any;
+
+    given('I am a consumer of the vehicles API', () => {
+      requestUrl = 'vehicles/ABCDEFGH654321/tech-records';
+    });
+    when('I call the vehicles API via the POST method with unexpected values for a field that accepts only specific values', async () => {
+      response = await request.get(requestUrl);
+    });
+    then('I am given the 400 error code', () => {
+      expect(response.status).toEqual(200);
+      expect(response.body.techRecord[0].statusCode).toEqual("provisional");
+      expect(response.body.techRecord[0]).toHaveProperty("grossEecWeight");
+      expect(response.body.techRecord[0]).toHaveProperty("dtpNumber");
+      expect(response.body.techRecord[0]).toHaveProperty("make");
+      expect(response.body.techRecord[0]).toHaveProperty("model");
+
+    });
+    ctx.succeed('done');
+    ctx = null;
+  });
+
+  test('AC4. POST: Attempt to create a new vehicle, using a field which has a field value outside of the min/max length for that field', ({given, when, then}) => {
+    let ctx: any = mockContext(opts);
+
+    let requestUrl: string;
+    let response: any;
+
+    given('I am a consumer of the vehicles API', () => {
+      requestUrl = 'vehicles/ABCDEFGH654321/tech-records';
+    });
+    when('I call the vehicles API via the POST method using a field which has a field value outside of the min/max length for that field', async () => {
+      response = await request.get(requestUrl);
+    });
+    then('I am given the 400 error code', () => {
+      expect(response.status).toEqual(200);
+      expect(response.body.techRecord[0].statusCode).toEqual("provisional");
+      expect(response.body.techRecord[0]).toHaveProperty("grossEecWeight");
+      expect(response.body.techRecord[0]).toHaveProperty("dtpNumber");
+      expect(response.body.techRecord[0]).toHaveProperty("make");
+      expect(response.body.techRecord[0]).toHaveProperty("model");
+
+    });
+    ctx.succeed('done');
+    ctx = null;
+  });
+});
+
+const createPOSTPayload = () => {
+  const techRec: any = cloneDeep(mockData[43]);
+  delete techRec.techRecord[0].statusCode;
+  const payload = {
+    msUserDetails: {
+      msUser: "dorel",
+      msOid: "1234545"
+    },
+    vin: Date.now().toString(),
+    primaryVrm: "ALKH567",
+    techRecord: techRec.techRecord
+  };
+  return payload;
+};
