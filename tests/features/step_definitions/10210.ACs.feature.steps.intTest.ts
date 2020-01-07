5ee87a2623b503420491574629011df38cbc8e56
Catalin-Andrei Stancu <castancu@deloittece.com>
2020-01-07 14:35:01 +0200

    fixed linting errors

--- tests/features/step_definitions/10210.ACs.feature.steps.intTest.ts
+++ tests/features/step_definitions/10210.ACs.feature.steps.intTest.ts
@@ -1,6 +1,6 @@
-import {defineFeature, loadFeature} from 'jest-cucumber';
+import {defineFeature, loadFeature} from "jest-cucumber";
 import supertest from "supertest";
-import path from 'path';
+import path from "path";
 import mockData from "../../resources/technical-records.json";
 import mockContext from "aws-lambda-mock-context";
 import {emptyDatabase, populateDatabase} from "../../util/dbOperations";
@@ -16,7 +16,7 @@ const opts = Object.assign({
 
 const feature = loadFeature(path.resolve(__dirname, "../10210.ACs.feature"));
 
-defineFeature(feature, test => {
+defineFeature(feature, (test) => {
   beforeAll(async () => {
     await emptyDatabase();
   });
@@ -33,91 +33,91 @@ defineFeature(feature, test => {
     await populateDatabase();
   });
 
-  test('AC1. POST: Attempt to create a new vehicle without a mandatory field', ({given, when, then}) => {
+  test("AC1. POST: Attempt to create a new vehicle without a mandatory field", ({given, when, then}) => {
     let ctx: any = mockContext(opts);
 
     let requestUrl: string;
     let response: any;
 
-    given('I am a consumer of the vehicles API', () => {
-      requestUrl = 'vehicles';
+    given("I am a consumer of the vehicles API", () => {
+      requestUrl = "vehicles";
     });
-    when('I call the vehicles API via the POST method without a mandatory field in the request body', async () => {
+    when("I call the vehicles API via the POST method without a mandatory field in the request body", async () => {
       const postPayload = createPOSTPayload();
       delete postPayload.techRecord[0].manufactureYear;
       response = await request.post(requestUrl).send(postPayload);
     });
-    then('I am given the 400 error code', () => {
+    then("I am given the 400 error code", () => {
       expect(response.status).toEqual(400);
       expect(response.body[0].message).toEqual('"manufactureYear" is required');
     });
-    ctx.succeed('done');
+    ctx.succeed("done");
     ctx = null;
   });
 
-  test('AC2. POST: Attempt to create a new vehicle with a not applicable field', ({given, when, then}) => {
+  test("AC2. POST: Attempt to create a new vehicle with a not applicable field", ({given, when, then}) => {
     let ctx: any = mockContext(opts);
 
     let requestUrl: string;
     let response: any;
 
-    given('I am a consumer of the vehicles API', () => {
-      requestUrl = 'vehicles';
+    given("I am a consumer of the vehicles API", () => {
+      requestUrl = "vehicles";
     });
     when('I call the vehicles API via the POST method with at least one not applicable field (for example, a "PSV only" field, onto a HGV)', async () => {
       const postPayload = createPOSTPayload();
       postPayload.techRecord[0].unladenWeight = 0;
       response = await request.post(requestUrl).send(postPayload);
     });
-    then('I am given the 400 error code', () => {
+    then("I am given the 400 error code", () => {
       expect(response.status).toEqual(400);
       expect(response.body[0].message).toEqual('"unladenWeight" is not allowed');
     });
-    ctx.succeed('done');
+    ctx.succeed("done");
     ctx = null;
   });
 
-  test('AC3. POST: Attempt to create a new vehicle with unexpected values for a field that accepts only specific values', ({given, when, then}) => {
+  test("AC3. POST: Attempt to create a new vehicle with unexpected values for a field that accepts only specific values", ({given, when, then}) => {
     let ctx: any = mockContext(opts);
 
     let requestUrl: string;
     let response: any;
 
-    given('I am a consumer of the vehicles API', () => {
-      requestUrl = 'vehicles';
+    given("I am a consumer of the vehicles API", () => {
+      requestUrl = "vehicles";
     });
-    when('I call the vehicles API via the POST method with unexpected values for a field that accepts only specific values', async () => {
+    when("I call the vehicles API via the POST method with unexpected values for a field that accepts only specific values", async () => {
       const postPayload = createPOSTPayload();
       postPayload.techRecord[0].fuelPropulsionSystem = "biscuit";
       response = await request.post(requestUrl).send(postPayload);
     });
-    then('I am given the 400 error code', () => {
+    then("I am given the 400 error code", () => {
       expect(response.status).toEqual(400);
       expect(response.body[0].message).toEqual('"fuelPropulsionSystem" must be one of [DieselPetrol, Hybrid, Electric, CNG, Fuel cell, LNG, Other]');
     });
-    ctx.succeed('done');
+    ctx.succeed("done");
     ctx = null;
   });
 
-  test('AC4. POST: Attempt to create a new vehicle, using a field which has a field value outside of the min/max length for that field', ({given, when, then}) => {
+  test("AC4. POST: Attempt to create a new vehicle, using a field which has a field value outside of the min/max length for that field", ({given, when, then}) => {
     let ctx: any = mockContext(opts);
 
     let requestUrl: string;
     let response: any;
 
-    given('I am a consumer of the vehicles API', () => {
-      requestUrl = 'vehicles';
+    given("I am a consumer of the vehicles API", () => {
+      requestUrl = "vehicles";
     });
-    when('I call the vehicles API via the POST method using a field which has a field value outside of the min/max length for that field', async () => {
+    when("I call the vehicles API via the POST method using a field which has a field value outside of the min/max length for that field", async () => {
       const postPayload = createPOSTPayload();
       postPayload.techRecord[0].manufactureYear = 123456;
       response = await request.post(requestUrl).send(postPayload);
     });
-    then('I am given the 400 error code', () => {
+    then("I am given the 400 error code", () => {
       expect(response.status).toEqual(400);
       expect(response.body[0].message).toEqual('"manufactureYear" must be less than or equal to 9999');
     });
-    ctx.succeed('done');
+    ctx.succeed("done");
     ctx = null;
   });
 });
