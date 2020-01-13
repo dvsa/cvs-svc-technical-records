44b295e26d3f40efdcb4b6053d82dff29ba89e41
Catalin-Andrei Stancu <castancu@deloittece.com>
2020-01-13 12:54:10 +0200

    finished the implementation + tests

---
+++ tests/features/step_definitions/10214.ACs.feature.steps.intTest.ts
@@ -0,0 +1,104 @@
+import {defineFeature, loadFeature} from "jest-cucumber";
+import supertest from "supertest";
+import path from "path";
+import mockData from "../../resources/technical-records.json";
+import mockContext from "aws-lambda-mock-context";
+import {emptyDatabase, populateDatabase} from "../../util/dbOperations";
+import {cloneDeep} from "lodash";
+
+const url = "http://localhost:3005/";
+const request = supertest(url);
+const opts = Object.assign({
+  timeout: 1.5
+});
+
+const createPUTPayload = () => {
+  const techRec: any = cloneDeep(mockData[43]);
+  delete techRec.techRecord[0].statusCode;
+  const payload = {
+    msUserDetails: {
+      msUser: "dorel",
+      msOid: "1234545"
+    },
+    techRecord: techRec.techRecord
+  };
+  return payload;
+};
+
+const feature = loadFeature(path.resolve(__dirname, "../10214.ACs.feature"));
+
+defineFeature(feature, (test) => {
+  beforeAll(async () => {
+    await emptyDatabase();
+  });
+
+  beforeEach(async () => {
+    await populateDatabase();
+  });
+
+  afterEach(async () => {
+    await emptyDatabase();
+  });
+
+  afterAll(async () => {
+    await populateDatabase();
+  });
+
+  test("AC1. PUT: Vehicle class code is autopopulated", ({given, when, then, and}) => {
+    let ctx: any = mockContext(opts);
+
+    let requestUrlPUT: string;
+    let requestUrlGET: string;
+    const putPayload = createPUTPayload();
+    let responsePUT: any;
+    let responseGET: any;
+
+    given("I am a consumer of the vehicles API", () => {
+      requestUrlPUT = "vehicles/ABCDEFGH654321";
+      requestUrlGET = `vehicles/ABCDEFGH654321/tech-records?status=all`;
+    });
+    and("I have completed the \"vehicle class description\" field", () => {
+      putPayload.techRecord[0].vehicleClass = {
+        description: "MOT class 4"
+      };
+    });
+    when("I submit my request via the PUT method", async () => {
+      responsePUT = await request.put(requestUrlPUT).send(putPayload);
+    });
+    then("the corresponding vehicle class code is autopopulated, as per the linked excel", async () => {
+      responseGET = await request.get(requestUrlGET);
+      expect(responseGET.body.techRecord[1].vehicleClass.code).toEqual("4");
+    });
+    ctx.succeed("done");
+    ctx = null;
+  });
+
+  test("AC2. PUT: Body type code is autopopulated", ({given, when, then, and}) => {
+    let ctx: any = mockContext(opts);
+
+    let requestUrlPUT: string;
+    let requestUrlGET: string;
+    const putPayload = createPUTPayload();
+    let responsePUT: any;
+    let responseGET: any;
+
+    given("I am a consumer of the vehicles API", () => {
+      requestUrlPUT = "vehicles/ABCDEFGH654321";
+      requestUrlGET = `vehicles/ABCDEFGH654321/tech-records?status=all`;
+    });
+    and("I have completed the \"body type description\" field", () => {
+      putPayload.techRecord[0].bodyType = {
+        description: "skeletal"
+      };
+    });
+    when("I submit my request via the PUT method", async () => {
+      responsePUT = await request.put(requestUrlPUT).send(putPayload);
+    });
+    then("the corresponding body type code is autopopulated, as per the linked excel", async () => {
+      responseGET = await request.get(requestUrlGET);
+      expect(responseGET.body.techRecord[1].bodyType.code).toEqual("k");
+    });
+    ctx.succeed("done");
+    ctx = null;
+  });
+});
