3323e842395dd71bf278aa7683ea98ac2eeb7862
Catalin-Andrei Stancu <castancu@deloittece.com>
2020-01-28 12:56:55 +0200

    added 2 new anonymised records in the seed file

--- tests/unit/insertTechRecordsList.unitTest.ts
+++ tests/unit/insertTechRecordsList.unitTest.ts
@@ -29,7 +29,7 @@ describe("insertTechRecordsList", () => {
 
       // @ts-ignore
       const data: ITechRecord[] = await techRecordsService.insertTechRecordsList(records);
-      expect(data.length).toEqual(70);
+      expect(data.length).toEqual(72);
     });
 
     it("should return nothing", async () => {
