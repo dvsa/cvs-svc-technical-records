a87928ae1a25ab12699205b92ed5495814312fdc
Catalin-Andrei Stancu <castancu@deloittece.com>
2020-01-29 12:57:04 +0200

    adjust vin min and max length

--- src/functions/updateTechRecords.ts
+++ src/functions/updateTechRecords.ts
@@ -16,7 +16,7 @@ const updateTechRecords = (event: any) => {
   const vin = event.pathParameters ? event.pathParameters.vin : null;
   const filesToUpload: string[] = event.body ? event.body.files : null;
 
-  if (!vin || !ONLY_DIGITS_AND_NUMBERS.test(vin) || vin.length < 9) {
+  if (!vin || !ONLY_DIGITS_AND_NUMBERS.test(vin) || vin.length < 9 || vin.length > 21) {
     return Promise.resolve(new HTTPResponse(400, "Invalid path parameter 'vin'"));
   }
 
