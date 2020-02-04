43461f6f995aab7110b76821f1e624c6e32dd16a
Catalin-Andrei Stancu <castancu@deloittece.com>
2020-02-04 17:54:39 +0000

    fixed rebase issues

--- tests/features/step_definitions/10213.ACs.feature.steps.intTest.ts
+++ tests/features/step_definitions/10213.ACs.feature.steps.intTest.ts
@@ -22,7 +22,6 @@ const createPOSTPayload = () => {
     },
     primaryVrm: Math.floor(100000 + Math.random() * 900000).toString(),
     vin: Date.now().toString(),
-    primaryVrm: Math.floor(100000 + Math.random() * 900000).toString(),
     techRecord: techRec.techRecord
   };
   return payload;
