4013c66639c6df7e27c89f5c54648072cf2128e9
Catalin-Andrei Stancu <castancu@deloittece.com>
2020-02-18 12:55:49 +0200

    common schema and psv refactoring

--- src/utils/CommonSchema.ts
+++ src/utils/CommonSchema.ts
@@ -2,7 +2,6 @@
 const Joi = require("@hapi/joi")
   .extend(require("@hapi/joi-date"));
 
-import {adrValidation} from "./AdrValidation";
 import {
   approvalType,
   bodyTypeDescription, euVehicleCategory, fitmentCode,
@@ -10,36 +9,41 @@ import {
   vehicleConfiguration, vehicleType
 } from "./ValidationUtils";
 
-export const hgvValidation = Joi.object().keys({
+export const brakesSchema = Joi.object().keys({
+  dtpNumber: Joi.string().max(6).required(),
+}).required();
+
+export const weightsSchema = Joi.object().keys({
+  gbWeight: Joi.number().min(0).max(99999).required(),
+  designWeight: Joi.number().min(0).max(99999).required()
+}).required();
+
+export const tyresSchema = Joi.object().keys({
+  tyreCode: Joi.number().min(0).max(9999).required(),
+  tyreSize: Joi.string().max(12).required(),
+  plyRating: Joi.string().max(2).optional().allow(null),
+  fitmentCode: Joi.string().valid(...fitmentCode).required(),
+  dataTrAxles: Joi.number().min(0).max(999).optional().allow(null)
+}).required();
+
+export const axlesSchema = Joi.object().keys({
+  parkingBrakeMrk: Joi.boolean().optional().allow(null),
+  axleNumber: Joi.number().min(0).max(99999).required(),
+  weights: weightsSchema,
+  tyres: tyresSchema
+});
+
+export const commonSchema = Joi.object().keys({
   vehicleType: Joi.string().valid(...vehicleType).required(),
   regnDate: Joi.date().format("YYYY-MM-DD").raw().optional().allow(null),
   manufactureYear: Joi.number().min(0).max(9999).required(),
   noOfAxles: Joi.number().min(0).max(99).required(),
-  brakes: Joi.object().keys({
-    dtpNumber: Joi.string().max(6).required(),
-  }).required(),
-  axles: Joi.array().items(Joi.object().keys({
-    parkingBrakeMrk: Joi.boolean().optional().allow(null),
-    axleNumber: Joi.number().min(0).max(99999).required(),
-    weights: Joi.object().keys({
-      gbWeight: Joi.number().min(0).max(99999).required(),
-      eecWeight: Joi.number().min(0).max(99999).optional().allow(null),
-      designWeight: Joi.number().min(0).max(99999).required()
-    }).required(),
-    tyres: Joi.object().keys({
-      tyreCode: Joi.number().min(0).max(9999).required(),
-      tyreSize: Joi.string().max(12).required(),
-      plyRating: Joi.string().max(2).optional().allow(null),
-      fitmentCode: Joi.string().valid(...fitmentCode).required(),
-      dataTrAxles: Joi.number().min(0).max(999).optional().allow(null)
-    }).required(),
-  })).required(),
+  brakes: brakesSchema,
+  axles: Joi.array().items(axlesSchema).required(),
   speedLimiterMrk: Joi.boolean().required(),
   tachoExemptMrk: Joi.boolean().required(),
   euroStandard: Joi.string().required(),
   fuelPropulsionSystem: Joi.string().valid(...fuelPropulsionSystem).required(),
-  roadFriendly: Joi.boolean().required(),
-  drawbarCouplingFitted: Joi.boolean().required(),
   vehicleClass: Joi.object().keys({
     code: Joi.any().when("description", {
       is: Joi.string().valid(...vehicleClassDescription).required(),
@@ -49,8 +53,7 @@ export const hgvValidation = Joi.object().keys({
     description: Joi.string().valid(...vehicleClassDescription).required()
   }).required(),
   vehicleConfiguration: Joi.string().valid(...vehicleConfiguration).required(),
-  offRoad: Joi.boolean().optional().required(),
-  numberOfWheelsDriven: Joi.number().min(0).max(9999).required().allow(null),
+  numberOfWheelsDriven: Joi.number().min(0).max(9999).required(),
   euVehicleCategory: Joi.string().valid(...euVehicleCategory).required(),
   emissionsLimit: Joi.number().min(0).max(99).optional().allow(null),
   departmentalVehicleMarker: Joi.boolean().optional(),
@@ -60,8 +63,6 @@ export const hgvValidation = Joi.object().keys({
   ntaNumber: Joi.string().max(40).optional().allow(null),
   variantNumber: Joi.string().max(25).optional().allow(null),
   variantVersionNumber: Joi.string().max(35).optional().allow(null),
-  make: Joi.string().max(30).required(),
-  model: Joi.string().max(30).required(),
   bodyType: Joi.object().keys({
     code: Joi.any().when("description", {
       is: Joi.string().valid(...bodyTypeDescription).required(),
@@ -73,28 +74,8 @@ export const hgvValidation = Joi.object().keys({
   functionCode: Joi.string().max(1).optional().allow(null),
   conversionRefNo: Joi.string().max(10).optional().allow(null),
   grossGbWeight: Joi.number().min(0).max(99999).required(),
-  grossEecWeight: Joi.number().min(0).max(99999).optional().allow(null),
   grossDesignWeight: Joi.number().min(0).max(99999).required(),
-  trainGbWeight: Joi.number().min(0).max(99999).required(),
-  trainEecWeight: Joi.number().min(0).max(99999).optional().allow(null),
   trainDesignWeight: Joi.number().min(0).max(99999).optional().allow(null),
-  maxTrainGbWeight: Joi.number().min(0).max(99999).required(),
-  maxTrainEecWeight: Joi.number().min(0).max(99999).optional().allow(null),
-  maxTrainDesignWeight: Joi.number().min(0).max(99999).optional().allow(null),
-  tyreUseCode: Joi.string().max(2).optional().allow(null),
-  dimensions: Joi.object().keys({
-    length: Joi.number().min(0).max(99999).required(),
-    width: Joi.number().min(0).max(99999).required(),
-    axleSpacing: Joi.array().items(Joi.object().keys({
-      value: Joi.number().min(0).max(99999).optional().allow(null),
-      axles: Joi.string().optional()
-    })).optional()
-  }).required(),
-  frontAxleToRearAxle: Joi.number().min(0).max(99999).required(),
-  frontAxleTo5thWheelCouplingMin: Joi.number().min(0).max(99999).optional().allow(null),
-  frontAxleTo5thWheelCouplingMax: Joi.number().min(0).max(99999).optional().allow(null),
-  frontAxleTo5thWheelMin: Joi.number().min(0).max(99999).optional().allow(null),
-  frontAxleTo5thWheelMax: Joi.number().min(0).max(99999).optional().allow(null),
   applicantDetails: Joi.object().keys({
     name: Joi.string().max(150).required(),
     address1: Joi.string().max(60).required(),
@@ -116,9 +97,7 @@ export const hgvValidation = Joi.object().keys({
     plateReasonForIssue: Joi.string().valid(...plateReasonForIssue).optional().allow(null),
     plateIssuer: Joi.string().max(150).optional().allow(null)
   })).optional().allow(null),
-  notes: Joi.string().optional().allow(null),
   reasonForCreation: Joi.string().max(100).required(),
-  adrDetails: adrValidation,
   createdAt: Joi.string().optional().allow(null),
   createdByName: Joi.string().optional(),
   createdById: Joi.string().optional(),
