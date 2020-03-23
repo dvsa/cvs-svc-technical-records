71163e908e561e39c24716696ebbe001ca9fd063
Catalin-Andrei Stancu <castancu@deloittece.com>
2020-03-23 17:51:39 +0200

    removed console log

--- tests/unit/payloadValidation.unitTest.ts
+++ tests/unit/payloadValidation.unitTest.ts
@@ -55,7 +55,6 @@ describe("payloadValidation", () => {
 
       it("should pass the validation and return the validated payload for TRL", () => {
         const techRec: any = cloneDeep(mockData[78]);
-        console.log("VINUL PULII", techRec.vin);
         delete techRec.techRecord[0].statusCode;
         delete techRec.techRecord[0].createdByName;
         delete techRec.techRecord[0].createdAt;
