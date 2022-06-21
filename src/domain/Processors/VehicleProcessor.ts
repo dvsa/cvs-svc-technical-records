9bb82dbb02edb28657e747dee6e1a9b62f9f8a93
Daniel Searle <Daniel.searle1@DVSA.GOV.UKDaniel.searle1@DVSA.GOV.UKdaniel.searle1@dvsa.gov.uk>
2022-06-21 15:52:50 +0100

    incremented non archived tech recrod

--- src/domain/Processors/VehicleProcessor.ts
+++ src/domain/Processors/VehicleProcessor.ts
@@ -271,7 +271,7 @@ export abstract class VehicleProcessor<T extends Vehicle> {
     const nonArchivedTechRecord = techRecordWrapper.techRecord.filter(
       (techRecord) => techRecord.statusCode !== enums.STATUS.ARCHIVED
     );
-    if (nonArchivedTechRecord.length > 1) {
+    if (nonArchivedTechRecord.length > 2) {
       throw this.Error(
         400,
         enums.HTTPRESPONSE.EU_VEHICLE_CATEGORY_MORE_THAN_ONE_TECH_RECORD
