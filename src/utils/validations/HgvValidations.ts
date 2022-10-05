71a16e77cddc829fdd33e1496e811794ccb193e0
Thomas.Webb <SA_Thomas.Webb@dvsatest-cloud.uk>
2022-10-05 16:41:22 +0100

    fix(cb2-5689): changed optional allow null to null and empty string

--- src/utils/validations/HgvValidations.ts
+++ src/utils/validations/HgvValidations.ts
@@ -6,7 +6,7 @@ import {adrValidation} from "./AdrValidation";
 export const hgvValidation = commonSchema.keys({
   axles: Joi.array().items(axlesSchema.keys({
     weights: weightsSchema.keys({
-      eecWeight: Joi.number().min(0).max(99999).optional().allow(null)
+      eecWeight: Joi.number().min(0).max(99999).optional().allow([null, ''])
     }).required(),
   })).min(1).required(),
   roadFriendly: Joi.boolean().required(),
@@ -19,28 +19,28 @@ export const hgvValidation = commonSchema.keys({
   euroStandard: Joi.string().required(),
   fuelPropulsionSystem: Joi.string().valid(...FUEL_PROPULSION_SYSTEM).required(),
   numberOfWheelsDriven: Joi.number().min(0).max(9999).required().allow(null),
-  emissionsLimit: Joi.number().min(0).max(99).optional().allow(null),
-  trainDesignWeight: Joi.number().min(0).max(99999).optional().allow(null),
-  grossEecWeight: Joi.number().min(0).max(99999).optional().allow(null),
+  emissionsLimit: Joi.number().min(0).max(99).optional().allow([null, '']),
+  trainDesignWeight: Joi.number().min(0).max(99999).optional().allow([null, '']),
+  grossEecWeight: Joi.number().min(0).max(99999).optional().allow([null, '']),
   trainGbWeight: Joi.number().min(0).max(99999).required(),
-  trainEecWeight: Joi.number().min(0).max(99999).optional().allow(null),
+  trainEecWeight: Joi.number().min(0).max(99999).optional().allow([null, '']),
   maxTrainGbWeight: Joi.number().min(0).max(99999).required(),
-  maxTrainEecWeight: Joi.number().min(0).max(99999).optional().allow(null),
-  maxTrainDesignWeight: Joi.number().min(0).max(99999).optional().allow(null),
-  tyreUseCode: Joi.string().max(2).optional().allow(null),
+  maxTrainEecWeight: Joi.number().min(0).max(99999).optional().allow([null, '']),
+  maxTrainDesignWeight: Joi.number().min(0).max(99999).optional().allow([null, '']),
+  tyreUseCode: Joi.string().max(2).optional().allow([null, '']),
   dimensions: Joi.object().keys({
     length: Joi.number().min(0).max(99999).required(),
     width: Joi.number().min(0).max(99999).required(),
     axleSpacing: Joi.array().items(Joi.object().keys({
-      value: Joi.number().min(0).max(99999).optional().allow(null),
+      value: Joi.number().min(0).max(99999).optional().allow([null, '']),
       axles: Joi.string().optional()
     })).optional()
   }).required(),
   frontAxleToRearAxle: Joi.number().min(0).max(99999).required(),
-  frontAxleTo5thWheelCouplingMin: Joi.number().min(0).max(99999).optional().allow(null),
-  frontAxleTo5thWheelCouplingMax: Joi.number().min(0).max(99999).optional().allow(null),
-  frontAxleTo5thWheelMin: Joi.number().min(0).max(99999).optional().allow(null),
-  frontAxleTo5thWheelMax: Joi.number().min(0).max(99999).optional().allow(null),
-  notes: Joi.string().optional().allow(null),
+  frontAxleTo5thWheelCouplingMin: Joi.number().min(0).max(99999).optional().allow([null, '']),
+  frontAxleTo5thWheelCouplingMax: Joi.number().min(0).max(99999).optional().allow([null, '']),
+  frontAxleTo5thWheelMin: Joi.number().min(0).max(99999).optional().allow([null, '']),
+  frontAxleTo5thWheelMax: Joi.number().min(0).max(99999).optional().allow([null, '']),
+  notes: Joi.string().optional().allow([null, '']),
   adrDetails: adrValidation
 }).required();
--- src/utils/validations/TrlValidations.ts
+++ src/utils/validations/TrlValidations.ts
@@ -7,51 +7,51 @@ import {FRAME_DESCRIPTION, LETTER_TYPE} from "../../assets/Enums";
 import {adrValidation} from "./AdrValidation";
 
 const authIntoService = Joi.object().keys({
-  cocIssueDate: Joi.date().format("YYYY-MM-DD").raw().optional().allow(null),
-  dateReceived: Joi.date().format("YYYY-MM-DD").raw().optional().allow(null),
-  datePending: Joi.date().format("YYYY-MM-DD").raw().optional().allow(null),
-  dateAuthorised: Joi.date().format("YYYY-MM-DD").raw().optional().allow(null),
-  dateRejected: Joi.date().format("YYYY-MM-DD").raw().optional().allow(null)
-}).optional().allow(null);
+  cocIssueDate: Joi.date().format("YYYY-MM-DD").raw().optional().allow([null, '']),
+  dateReceived: Joi.date().format("YYYY-MM-DD").raw().optional().allow([null, '']),
+  datePending: Joi.date().format("YYYY-MM-DD").raw().optional().allow([null, '']),
+  dateAuthorised: Joi.date().format("YYYY-MM-DD").raw().optional().allow([null, '']),
+  dateRejected: Joi.date().format("YYYY-MM-DD").raw().optional().allow([null, ''])
+}).optional().allow([null, '']);
 
 const lettersOfAuth = Joi.object().keys({
-  letterType: Joi.string().valid(...LETTER_TYPE).optional().allow(null),
-  letterDateRequested: Joi.date().format("YYYY-MM-DD").raw().optional().allow(null),
-  letterContents: Joi.string().optional().allow(null)
-}).optional().allow(null);
+  letterType: Joi.string().valid(...LETTER_TYPE).optional().allow([null, '']),
+  letterDateRequested: Joi.date().format("YYYY-MM-DD").raw().optional().allow([null, '']),
+  letterContents: Joi.string().optional().allow([null, ''])
+}).optional().allow([null, '']);
 
 export const trlValidation = commonSchema.keys({
-  firstUseDate: Joi.date().format("YYYY-MM-DD").raw().optional().allow(null),
-  suspensionType: Joi.string().max(1).optional().allow(null),
-  couplingType: Joi.string().max(1).optional().allow(null),
-  maxLoadOnCoupling: Joi.number().min(0).max(99999).optional().allow(null),
-  frameDescription: Joi.string().valid(...FRAME_DESCRIPTION).optional().allow(null),
+  firstUseDate: Joi.date().format("YYYY-MM-DD").raw().optional().allow([null, '']),
+  suspensionType: Joi.string().max(1).optional().allow([null, '']),
+  couplingType: Joi.string().max(1).optional().allow([null, '']),
+  maxLoadOnCoupling: Joi.number().min(0).max(99999).optional().allow([null, '']),
+  frameDescription: Joi.string().valid(...FRAME_DESCRIPTION).optional().allow([null, '']),
   authIntoService: authIntoService,
   lettersOfAuth: lettersOfAuth,
   make: Joi.string().max(30).required(),
   model: Joi.string().max(30).required(),
-  grossEecWeight: Joi.number().min(0).max(99999).optional().allow(null),
+  grossEecWeight: Joi.number().min(0).max(99999).optional().allow([null, '']),
   axles: Joi.array().items(axlesSchema.keys({
     weights: weightsSchema.keys({
-      eecWeight: Joi.number().min(0).max(99999).optional().allow(null)
+      eecWeight: Joi.number().min(0).max(99999).optional().allow([null, ''])
     }).required(),
     brakes: Joi.object().keys({
-      brakeActuator: Joi.number().min(0).max(999).optional().allow(null),
-      leverLength: Joi.number().min(0).max(999).optional().allow(null),
-      springBrakeParking: Joi.boolean().optional().allow(null)
-    }).optional().allow(null)
+      brakeActuator: Joi.number().min(0).max(999).optional().allow([null, '']),
+      leverLength: Joi.number().min(0).max(999).optional().allow([null, '']),
+      springBrakeParking: Joi.boolean().optional().allow([null, ''])
+    }).optional().allow([null, ''])
   })).min(1).required(),
   roadFriendly: Joi.boolean().required(),
-  tyreUseCode: Joi.string().max(2).optional().allow(null),
+  tyreUseCode: Joi.string().max(2).optional().allow([null, '']),
   brakes: brakesSchema.keys({
-    loadSensingValve: Joi.boolean().optional().allow(null),
+    loadSensingValve: Joi.boolean().optional().allow([null, '']),
     antilockBrakingSystem: Joi.boolean().required()
   }).required(),
   dimensions: Joi.object().keys({
     length: Joi.number().min(0).max(99999).required(),
     width: Joi.number().min(0).max(99999).required(),
     axleSpacing: Joi.array().items(Joi.object().keys({
-      value: Joi.number().min(0).max(99999).optional().allow(null),
+      value: Joi.number().min(0).max(99999).optional().allow([null, '']),
       axles: Joi.string().optional()
     })).optional()
   }).required(),
@@ -63,13 +63,13 @@ export const trlValidation = commonSchema.keys({
   couplingCenterToRearTrlMax: Joi.number().min(0).max(99999).required(),
   centreOfRearmostAxleToRearOfTrl: Joi.number().min(0).max(99999).required(),
   purchaserDetails: applicantDetailsSchema.keys({
-    faxNumber: Joi.string().max(25).optional().allow(null),
-    purchaserNotes: Joi.string().max(1024).optional().allow(null)
+    faxNumber: Joi.string().max(25).optional().allow([null, '']),
+    purchaserNotes: Joi.string().max(1024).optional().allow([null, ''])
   }).required(),
   manufacturerDetails: applicantDetailsSchema.keys({
-    faxNumber: Joi.string().max(25).optional().allow(null),
-    manufacturerNotes: Joi.string().max(1024).optional().allow(null)
+    faxNumber: Joi.string().max(25).optional().allow([null, '']),
+    manufacturerNotes: Joi.string().max(1024).optional().allow([null, ''])
   }).required(),
-  notes: Joi.string().optional().allow(null),
+  notes: Joi.string().optional().allow([null, '']),
   adrDetails: adrValidation
 }).required();
