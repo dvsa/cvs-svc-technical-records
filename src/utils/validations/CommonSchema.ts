b368600051c15fd849c93e971dfed8cb62422f20
Aaron Downing <aaron.downing@bjss.com>
2022-10-06 15:16:17 +0100

    fix(cb2-6238): make all optional null fields also allow empty strings

--- src/utils/validations/CommonSchema.ts
+++ src/utils/validations/CommonSchema.ts
@@ -28,29 +28,29 @@ export const weightsSchema = Joi.object().keys({
 export const tyresSchema = Joi.object().keys({
   tyreCode: Joi.number().min(0).max(9999).required(),
   tyreSize: Joi.string().max(12).required(),
-  plyRating: Joi.string().max(2).optional().allow(null),
+  plyRating: Joi.string().max(2).optional().allow(null, ''),
   fitmentCode: Joi.string().valid(...FITMENT_CODE).required(),
   dataTrAxles: Joi.number().min(0).max(999).optional().allow(null)
 }).required();
 
 export const axlesSchema = Joi.object().keys({
-  parkingBrakeMrk: Joi.boolean().optional().allow(null),
+  parkingBrakeMrk: Joi.boolean().optional().allow(null, ''),
   axleNumber: Joi.number().min(0).max(99999).required(),
   weights: weightsSchema,
   tyres: tyresSchema
 });
 
 export const platesSchema = Joi.object().keys({
-  plateSerialNumber: Joi.string().max(12).optional().allow(null),
-  plateIssueDate: Joi.date().format("YYYY-MM-DD").raw().optional().allow(null),
-  plateReasonForIssue: Joi.string().valid(...PLATE_REASON_FOR_ISSUE).optional().allow(null),
-  plateIssuer: Joi.string().max(150).optional().allow(null)
+  plateSerialNumber: Joi.string().max(12).optional().allow(null, ''),
+  plateIssueDate: Joi.date().format("YYYY-MM-DD").raw().optional().allow(null, ''),
+  plateReasonForIssue: Joi.string().valid(...PLATE_REASON_FOR_ISSUE).optional().allow(null, ''),
+  plateIssuer: Joi.string().max(150).optional().allow(null, '')
 });
 
 export const microfilmSchema = Joi.object().keys({
-  microfilmDocumentType: Joi.string().valid(...MICROFILM_DOCUMENT_TYPE).optional().allow(null),
-  microfilmRollNumber: Joi.string().max(5).optional().allow(null),
-  microfilmSerialNumber: Joi.string().max(4).optional().allow(null)
+  microfilmDocumentType: Joi.string().valid(...MICROFILM_DOCUMENT_TYPE).optional().allow(null, ''),
+  microfilmRollNumber: Joi.string().max(5).optional().allow(null, ''),
+  microfilmSerialNumber: Joi.string().max(4).optional().allow(null, '')
 }).optional().allow(null);
 
 export const applicantDetailsSchema = Joi.object().keys({
@@ -58,10 +58,10 @@ export const applicantDetailsSchema = Joi.object().keys({
   address1: Joi.string().max(60).required(),
   address2: Joi.string().max(60).required(),
   postTown: Joi.string().max(60).required(),
-  address3: Joi.string().max(60).optional().allow(null),
-  postCode: Joi.string().max(12).optional().allow(null),
-  telephoneNumber: Joi.string().max(25).optional().allow(null),
-  emailAddress: Joi.string().max(255).optional().allow(null)
+  address3: Joi.string().max(60).optional().allow(null, ''),
+  postCode: Joi.string().max(12).optional().allow(null, ''),
+  telephoneNumber: Joi.string().max(25).optional().allow(null, ''),
+  emailAddress: Joi.string().max(255).optional().allow(null, '')
 }).required();
 
 export const applicantDetailsSchemaOptional = Joi.object().keys({
@@ -96,10 +96,10 @@ export const commonSchema = Joi.object().keys({
   departmentalVehicleMarker: Joi.boolean().optional(),
   alterationMarker: Joi.boolean().optional(),
   approvalType: Joi.string().valid(...APPROVAL_TYPE).required(),
-  approvalTypeNumber: Joi.string().max(25).optional().allow(null),
-  ntaNumber: Joi.string().max(40).optional().allow(null),
-  variantNumber: Joi.string().max(25).optional().allow(null),
-  variantVersionNumber: Joi.string().max(35).optional().allow(null),
+  approvalTypeNumber: Joi.string().max(25).optional().allow(null, ''),
+  ntaNumber: Joi.string().max(40).optional().allow(null, ''),
+  variantNumber: Joi.string().max(25).optional().allow(null, ''),
+  variantVersionNumber: Joi.string().max(35).optional().allow(null, ''),
   bodyType: Joi.object().keys({
     code: Joi.any().when("description", {
       is: Joi.string().valid(...BODY_TYPE_DESCRIPTION).required(),
@@ -108,18 +108,18 @@ export const commonSchema = Joi.object().keys({
     }),
     description: Joi.string().valid(...BODY_TYPE_DESCRIPTION).required()
   }).required(),
-  functionCode: Joi.string().max(1).optional().allow(null),
-  conversionRefNo: Joi.string().max(10).optional().allow(null),
+  functionCode: Joi.string().max(1).optional().allow(null, ''),
+  conversionRefNo: Joi.string().max(10).optional().allow(null, ''),
   grossGbWeight: Joi.number().min(0).max(99999).required(),
   grossDesignWeight: Joi.number().min(0).max(99999).required(),
   applicantDetails: applicantDetailsSchema,
   microfilm: microfilmSchema,
   plates: Joi.array().items(platesSchema).optional().allow(null),
   reasonForCreation: Joi.string().max(100).required(),
-  createdAt: Joi.string().optional().allow(null),
+  createdAt: Joi.string().optional().allow(null, ''),
   createdByName: Joi.string().optional(),
   createdById: Joi.string().optional(),
-  lastUpdatedAt: Joi.string().optional().allow(null),
+  lastUpdatedAt: Joi.string().optional().allow(null, ''),
   lastUpdatedByName: Joi.string().optional(),
   lastUpdatedById: Joi.string().optional(),
   statusCode: Joi.string().valid(...STATUS_CODES)
