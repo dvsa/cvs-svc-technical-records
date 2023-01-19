949184aed59a4c9178b2e503d46aec9366f499de
Nathan Brown <nathan.brown@bjss.com>
2023-01-19 10:03:50 +0000

    feat(cb2-7248): remove all required valdiation

--- src/utils/validations/LgvValidations.ts
+++ src/utils/validations/LgvValidations.ts
@@ -3,13 +3,13 @@ import {VEHICLE_CLASS_DESCRIPTION, VEHICLE_SUBCLASS} from "../../assets/Enums";
 import {commonSchemaLgvMotorcycleCar} from "./CommonSchema";
 
 export const lgvValidation = commonSchemaLgvMotorcycleCar.keys({
-  vehicleSubclass: Joi.array().items(Joi.string().valid(...VEHICLE_SUBCLASS)).required(),
+  vehicleSubclass: Joi.array().items(Joi.string().valid(...VEHICLE_SUBCLASS)),
   vehicleClass: Joi.object().keys({
     code: Joi.any().when("description", {
-      is: Joi.string().valid(...VEHICLE_CLASS_DESCRIPTION).required(),
+      is: Joi.string().valid(...VEHICLE_CLASS_DESCRIPTION),
       then: Joi.string().optional(),
       otherwise: Joi.object().forbidden()
     }),
-    description: Joi.string().valid(...VEHICLE_CLASS_DESCRIPTION).required()
+    description: Joi.string().valid(...VEHICLE_CLASS_DESCRIPTION)
   }).optional().allow(null)
-}).required();
+});
--- src/utils/validations/MotorcycleValidations.ts
+++ src/utils/validations/MotorcycleValidations.ts
@@ -3,13 +3,13 @@ import {commonSchemaLgvMotorcycleCar} from "./CommonSchema";
 import {VEHICLE_CLASS_DESCRIPTION} from "../../assets/Enums";
 
 export const motorcycleValidation = commonSchemaLgvMotorcycleCar.keys({
-  numberOfWheelsDriven: Joi.number().min(0).max(9999).required(),
+  numberOfWheelsDriven: Joi.number().min(0).max(9999),
   vehicleClass: Joi.object().keys({
     code: Joi.any().when("description", {
-      is: Joi.string().valid(...VEHICLE_CLASS_DESCRIPTION).required(),
+      is: Joi.string().valid(...VEHICLE_CLASS_DESCRIPTION),
       then: Joi.string().optional(),
       otherwise: Joi.object().forbidden()
     }),
-    description: Joi.string().valid(...VEHICLE_CLASS_DESCRIPTION).required()
-  }).required()
-}).required();
+    description: Joi.string().valid(...VEHICLE_CLASS_DESCRIPTION)
+  })
+});
