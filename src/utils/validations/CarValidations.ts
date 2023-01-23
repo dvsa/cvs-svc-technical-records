a79cb4da73c8fb3f47dbfa1096dad6ebef0305ef
Nathan Brown <nathan.brown@bjss.com>
2023-01-23 10:53:38 +0000

    feat(cb2-7248): undo validation changes to vheicle and body type

--- src/utils/validations/CarValidations.ts
+++ src/utils/validations/CarValidations.ts
@@ -6,10 +6,10 @@ export const carValidation = commonSchemaLgvMotorcycleCar.keys({
   vehicleSubclass: Joi.array().items(Joi.string().valid(...VEHICLE_SUBCLASS)),
   vehicleClass: Joi.object().keys({
     code: Joi.any().when("description", {
-      is: Joi.string().valid(...VEHICLE_CLASS_DESCRIPTION),
+      is: Joi.string().valid(...VEHICLE_CLASS_DESCRIPTION).required(),
       then: Joi.string().optional(),
       otherwise: Joi.object().forbidden()
     }),
-    description: Joi.string().valid(...VEHICLE_CLASS_DESCRIPTION)
+    description: Joi.string().valid(...VEHICLE_CLASS_DESCRIPTION).required()
   }).optional().allow(null)
-});
+}).required();
--- src/utils/validations/MotorcycleValidations.ts
+++ src/utils/validations/MotorcycleValidations.ts
@@ -6,10 +6,10 @@ export const motorcycleValidation = commonSchemaLgvMotorcycleCar.keys({
   numberOfWheelsDriven: Joi.number().min(0).max(9999),
   vehicleClass: Joi.object().keys({
     code: Joi.any().when("description", {
-      is: Joi.string().valid(...VEHICLE_CLASS_DESCRIPTION),
+      is: Joi.string().valid(...VEHICLE_CLASS_DESCRIPTION).required(),
       then: Joi.string().optional(),
       otherwise: Joi.object().forbidden()
     }),
-    description: Joi.string().valid(...VEHICLE_CLASS_DESCRIPTION)
-  })
-});
+    description: Joi.string().valid(...VEHICLE_CLASS_DESCRIPTION).required()
+  }).required()
+}).required();
