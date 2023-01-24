477e99904feffcc18f87241eb4251d58ebcc9674
Taha Yazici <105922152+tahayazici@users.noreply.github.com>
2023-01-24 14:40:52 +0000

    feat(cb2-397): add new vehicle body types (#242)
    * feat(cb2-397): add the vehicle body types
    
    * refactor(cb2-397): clean up the helper methods
    
    * test(cb2-397): fix broken unit tests

--- tests/unit/payloadValidation.unitTest.ts
+++ tests/unit/payloadValidation.unitTest.ts
@@ -1,44 +1,15 @@
 import {cloneDeep} from "lodash";
 import mockData from "../resources/technical-records.json";
 import ITechRecord from "../../@Types/ITechRecord";
-import * as fromValidation from "../../src/utils/validations";
 import {BODY_TYPE_DESCRIPTION, VEHICLE_CLASS_DESCRIPTION, ERRORS} from "../../src/assets/Enums";
-import Configuration from "../../src/utils/Configuration";
 import HTTPError from "../../src/models/HTTPError";
+import Configuration from "../../src/utils/Configuration";
+import * as fromValidation from "../../src/utils/validations";
 
 const createPayload = () => {
   const techRec: any = cloneDeep(mockData[74]);
   techRec.techRecord[0].reasonForCreation = "some reason for update";
   return techRec.techRecord[0];
-};
-
-const vehicleClassMap: any = {
-  "motorbikes over 200cc or with a sidecar": "2",
-  "not applicable": "n",
-  "small psv (ie: less than or equal to 22 seats)": "s",
-  "motorbikes up to 200cc": "1",
-  "trailer": "t",
-  "large psv(ie: greater than 23 seats)": "l",
-  "3 wheelers": "3",
-  "heavy goods vehicle": "v",
-  "MOT class 4": "4",
-  "MOT class 7": "7",
-  "MOT class 5": "5"
-};
-
-const bodyTypeMap: any = {
-  "articulated": "a",
-  "single decker": "s",
-  "double decker": "d",
-  "other": "o",
-  "petrol/oil tanker": "p",
-  "skeletal": "k",
-  "tipper": "t",
-  "box": "b",
-  "flat": "f",
-  "refuse": "r",
-  "skip loader": "s",
-  "refrigerated": "c"
 };
 
 describe("New vehicle classes creation", () => {
@@ -55,7 +26,7 @@ describe("New vehicle classes creation", () => {
         for (const vehicleClass of VEHICLE_CLASS_DESCRIPTION) {
           payload.vehicleClass.description = vehicleClass;
           fromValidation.populateFields(payload);
-          expect(payload.vehicleClass.code).toEqual(vehicleClassMap[vehicleClass]);
+          expect(payload.vehicleClass.code).toEqual(fromValidation.vehicleClassCodeMap.get(vehicleClass));
         }
       });
 
@@ -71,7 +42,7 @@ describe("New vehicle classes creation", () => {
         for (const bodyType of BODY_TYPE_DESCRIPTION) {
           payload.bodyType.description = bodyType;
           fromValidation.populateFields(payload);
-          expect(payload.bodyType.code).toEqual(bodyTypeMap[bodyType]);
+          expect(payload.bodyType.code).toEqual(fromValidation.bodyTypeCodeMap.get(bodyType));
         }
       });
 
