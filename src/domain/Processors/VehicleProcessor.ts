08372458ff0c99d05b86c646601404bcf0ce9e44
Mauricio Cadenas <102959178+mcadenas-bjss@users.noreply.github.com>
2023-02-08 10:21:15 +0000

    Update src/domain/Processors/VehicleProcessor.ts

--- src/domain/Processors/VehicleProcessor.ts
+++ src/domain/Processors/VehicleProcessor.ts
@@ -62,7 +62,6 @@ export abstract class VehicleProcessor<T extends Vehicle> {
    * @param updatedVehicle The updated tech record
    */
   protected updateVehicleIdentifiers(existingVehicle: T, updatedVehicle: T): T {
-    ;
     const { primaryVrm } = updatedVehicle;
     const previousPrimaryVrm = existingVehicle.primaryVrm;
     updatedVehicle.secondaryVrms = existingVehicle.secondaryVrms ? Object.assign([], existingVehicle.secondaryVrms) : undefined
