26862895606bb1bc537982b0ec2a58a28f6a497f
Taha Yazici <taha.yazici@bjss.com>
2023-03-01 15:59:17 +0000

    fix(cb2-6641): allow notes on small vehicles

--- src/utils/validations/MotorcycleValidations.ts
+++ src/utils/validations/MotorcycleValidations.ts
@@ -13,5 +13,6 @@ export const motorcycleValidation = commonSchemaLgvCarSmallTrlMotorcycle.keys({
       otherwise: Joi.object().forbidden()
     }),
     description: Joi.string().valid(...VEHICLE_CLASS_DESCRIPTION)
-  })
+  }),
+  notes: Joi.string().optional().allow(null, "")
 });
