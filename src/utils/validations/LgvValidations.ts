7c523a41d52ae2e1ecbbf4675ccc7fc85c4dc3f1
Mauricio Cadenas <mauricio.cadenas@bjss.com>
2023-03-02 17:13:00 +0000

    feat(cb2-7044): fix validation

--- src/utils/validations/LgvValidations.ts
+++ src/utils/validations/LgvValidations.ts
@@ -3,24 +3,24 @@ import { EU_VEHICLE_CATEGORY, STATUS_CODES, VEHICLE_SUBCLASS, VEHICLE_TYPE } fro
 import { commonSchemaLgvCarSmallTrlMotorcycle } from "./CommonSchema";
 
 export const lgvValidation = commonSchemaLgvCarSmallTrlMotorcycle.keys({
+  historicPrimaryVrm: Joi.string().optional(),
+  historicSecondaryVrms: Joi.array().items(Joi.string()).optional(),
   statusCode: Joi.string().valid(...STATUS_CODES).optional(),
   vehicleType: Joi.string().valid(...Object.values(VEHICLE_TYPE)).optional(),
-  regnDate: Joi.date().optional(),
-  manufactureYear: Joi.number().min(1900).max(9999).optional(),
-  noOfAxles: Joi.number().min(0).max(99).optional(),
+  regnDate: Joi.date().optional().allow(null, ""),
+  manufactureYear: Joi.number().min(1900).max(9999).optional().allow(null, ""),
+  noOfAxles: Joi.number().min(0).max(99).optional().allow(null, ""),
   vehicleSubclass: Joi.array().items(Joi.string().valid(...VEHICLE_SUBCLASS)).optional(),
-  euVehicleCategory: Joi.string().valid(...Object.values(EU_VEHICLE_CATEGORY)).optional(),
-
+  euVehicleCategory: Joi.string().valid(...Object.values(EU_VEHICLE_CATEGORY)).optional().allow(null, ""),
   applicantDetails: Joi.object().keys({
-    name: Joi.string().max(150).optional(),
-    address1: Joi.string().max(60).optional(),
-    address2: Joi.string().max(60).optional(),
-    address3: Joi.string().max(60).optional(),
-    postTown: Joi.string().max(60).optional(),
-    postCode: Joi.string().max(12).optional(),
-    emailAddress: Joi.string().max(255).optional(),
-    telephonenumber: Joi.string().max(25).optional(),
+    name: Joi.string().max(150).optional().allow(null, ""),
+    address1: Joi.string().max(60).optional().allow(null, ""),
+    address2: Joi.string().max(60).optional().allow(null, ""),
+    address3: Joi.string().max(60).optional().allow(null, ""),
+    postTown: Joi.string().max(60).optional().allow(null, ""),
+    postCode: Joi.string().max(12).optional().allow(null, ""),
+    emailAddress: Joi.string().max(255).optional().allow(null, ""),
+    telephoneNumber: Joi.string().max(25).optional().allow(null, ""),
   }).optional(),
-
-  notes: Joi.string().max(1024).optional()
+  notes: Joi.string().max(1024).optional().allow(null, ""),
 });
--- src/utils/validations/MotorcycleValidations.ts
+++ src/utils/validations/MotorcycleValidations.ts
@@ -1,20 +1,25 @@
 import Joi from "@hapi/joi";
+import { EU_VEHICLE_CATEGORY, STATUS_CODES, VEHICLE_CLASS_DESCRIPTION, VEHICLE_TYPE } from "../../assets/Enums";
 import { commonSchemaLgvCarSmallTrlMotorcycle } from "./CommonSchema";
-import { EU_VEHICLE_CATEGORY, STATUS_CODES, VEHICLE_CLASS_CODE, VEHICLE_CLASS_DESCRIPTION, VEHICLE_TYPE } from "../../assets/Enums";
 
 export const motorcycleValidation = commonSchemaLgvCarSmallTrlMotorcycle.keys({
+  historicPrimaryVrm: Joi.string().optional(),
+  historicSecondaryVrms: Joi.array().items(Joi.string()).optional(),
   statusCode: Joi.string().valid(...STATUS_CODES).optional(),
   vehicleType: Joi.string().valid(...Object.values(VEHICLE_TYPE)).optional(),
-  regnDate: Joi.date().optional(),
-  manufactureYear: Joi.number().min(1900).max(9999).optional(),
-  noOfAxles: Joi.number().min(0).max(99).optional(),
+  regnDate: Joi.date().optional().allow(null, ""),
+  manufactureYear: Joi.number().min(1900).max(9999).optional().allow(null, ""),
+  noOfAxles: Joi.number().min(0).max(99).optional().allow(null, ""),
   vehicleClass: Joi.object().keys({
-    code: Joi.string().valid(...VEHICLE_CLASS_CODE).optional(),
-    description: Joi.string().valid(...VEHICLE_CLASS_DESCRIPTION).optional()
-  }).optional(),
-  euVehicleCategory: Joi.string().valid(...Object.values(EU_VEHICLE_CATEGORY)).optional(),
-  numberOfWheelsDriven: Joi.number().min(0).max(9999).optional(),
-
+    code: Joi.any().when("description", {
+      is: Joi.string().valid(...VEHICLE_CLASS_DESCRIPTION),
+      then: Joi.string().optional(),
+      otherwise: Joi.object().forbidden()
+    }),
+    description: Joi.string().valid(...VEHICLE_CLASS_DESCRIPTION)
+  }).optional().allow(null),
+  euVehicleCategory: Joi.string().valid(...Object.values(EU_VEHICLE_CATEGORY)).optional().allow(null, ""),
+  numberOfWheelsDriven: Joi.number().min(0).max(9999).optional().allow(null, ''),
   applicantDetails: Joi.object().keys({
     name: Joi.string().max(150).optional(),
     address1: Joi.string().max(60).optional(),
@@ -23,8 +28,7 @@ export const motorcycleValidation = commonSchemaLgvCarSmallTrlMotorcycle.keys({
     postTown: Joi.string().max(60).optional(),
     postCode: Joi.string().max(12).optional(),
     emailAddress: Joi.string().max(255).optional(),
-    telephonenumber: Joi.string().max(25).optional(),
+    telephoneNumber: Joi.string().max(25).optional(),
   }).optional(),
-
-  notes: Joi.string().max(1024).optional()
+  notes: Joi.string().max(1024).optional().allow(null, "")
 });
