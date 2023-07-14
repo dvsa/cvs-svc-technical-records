ed20491a0e4d55002e3531654d1962623b794822
Nathan Brown <nathan.brown@bjss.com>
2023-07-14 14:43:23 +0100

    feat(cb2-8661): remove validation as moved to earlier step (#329)
    * feat(cb2-8661): remove validation as moved to earlier step
    
    * feat(cb2-8661): remove old int test

--- tests/integration/updateTechRecordStatus.intTest.ts
+++ tests/integration/updateTechRecordStatus.intTest.ts
@@ -23,29 +23,6 @@ describe("UpdateTechRecordStatus", () => {
     });
 
     context("when trying to update a vehicle", () => {
-        it("should not update the status if the test type does not require a tech record update", async () => {
-            const systemNumber: string = "11000001";
-            expect.assertions(2);
-            await LambdaTester(updateTechRecordStatus)
-                .event({
-                    path: "/vehicles/update-status/" + systemNumber,
-                    pathParameters: {
-                      systemNumber,
-                    },
-                    queryStringParameters: {
-                        testStatus: "submitted",
-                        testResult: "pass",
-                        testTypeId: "1",
-                    },
-                    httpMethod: "PUT",
-                    resource: "/vehicles/update-status/{systemNumber}"
-                })
-                .expectResolve((result: any) => {
-                    expect(result.statusCode).toBe(200);
-                    expect(JSON.parse(result.body)).toBe(HTTPRESPONSE.NO_STATUS_UPDATE_REQUIRED);
-                });
-        });
-
         it("should return 200 and the updated vehicle if it has a provisional techRecord", async () => {
             const systemNumber: string = "11000027";
             expect.assertions(4);
